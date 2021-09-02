20edbd6959d940ae4873479945a48d3235b32a60
Alexander Chobanov <alexchobanov@gmail.com>
2021-09-02 23:40:42 +0300

    Added pproper scrollbar support for lists

---
+++ src/Gateways/Desktop/Core/Services/Implementations/MouseInputService.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace EnduranceJudge.Gateways.Desktop.Core.Services.Implementations
+{
+    public class MouseInputService : IMouseInputService
+    {
+        public void HandleScroll(object sender, MouseWheelEventArgs scrollEvent)
+        {
+            var scv = (ScrollViewer)sender;
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - scrollEvent.Delta);
+            scrollEvent.Handled = true;
+        }
+    }
+}
--- src/Gateways/Desktop/Views/Content/Event/Roots/Horses/Listing/HorseListView.xaml.cs
+++ src/Gateways/Desktop/Views/Content/Event/Roots/Horses/Listing/HorseListView.xaml.cs
@@ -1,15 +1,29 @@
 ﻿using EnduranceJudge.Gateways.Desktop.Core;
+using EnduranceJudge.Gateways.Desktop.Core.Services;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace EnduranceJudge.Gateways.Desktop.Views.Content.Event.Roots.Horses.Listing
 {
     public partial class HorseListView : UserControl, IView
     {
+        private readonly IMouseInputService mouseInput;
+
         public HorseListView()
         {
+        }
+
+        public HorseListView(IMouseInputService mouseInput)
+        {
+            this.mouseInput = mouseInput;
             InitializeComponent();
         }
 
         public string RegionName { get; } = Regions.Content;
+
+        private void HandleScroll(object sender, MouseWheelEventArgs mouseEvent)
+        {
+            this.mouseInput.HandleScroll(sender, mouseEvent);
+        }
     }
 }
