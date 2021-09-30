d8f55f35df1538fdc7a386a904d87896d1d65705
Alexander Chobanov <alexchobanov@gmail.com>
2021-09-30 20:50:34 +0300

    Fixed future date validation

--- src/Localization/Strings.cs
+++ src/Localization/Strings.cs
@@ -9,6 +9,8 @@
             public const string CannotRemoveItemIsNotFoundTemplate = "cannot remove '{0}' - it is not found.";
             public const string CannotAddNullItemTemplate = "cannot add 'null' to a collection.";
             public const string CannotAddItemExistsTemplate = "cannot add '{0}' because entity with Id '{1}' already exists.";
+            public const string INVALID_FUTURE_DATE_TEMPLATE = "Date '{0}' is not future date.";
+
 
             public static class Ranking
             {
