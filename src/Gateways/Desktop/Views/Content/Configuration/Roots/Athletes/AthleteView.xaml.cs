3d817ffdda3e23aa96b6d3e693b6e79e001505ee
Alexander Chobanov <alexander.chobanov@dowjones.com>
2022-02-04 17:27:10 +0200

    Implement IView.HangleScroll to fix errors

--- src/Gateways/Desktop/Views/Content/Configuration/Roots/Athletes/AthleteView.xaml.cs
+++ src/Gateways/Desktop/Views/Content/Configuration/Roots/Athletes/AthleteView.xaml.cs
@@ -1,15 +1,28 @@
 ﻿using EnduranceJudge.Gateways.Desktop.Core;
+using EnduranceJudge.Gateways.Desktop.Core.Services;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace EnduranceJudge.Gateways.Desktop.Views.Content.Configuration.Roots.Athletes
 {
     public partial class AthleteView : UserControl, IView
     {
+        private readonly IInputHandler inputHandler;
+        public AthleteView(IInputHandler inputHandler) : this()
+        {
+            this.inputHandler = inputHandler;
+            this.inputHandler = inputHandler;
+        }
         public AthleteView()
         {
             InitializeComponent();
         }
 
         public string RegionName { get; } = Regions.CONTENT_LEFT;
+
+        public void HandleScroll(object sender, MouseWheelEventArgs args)
+        {
+            this.inputHandler.HandleScroll(sender, args);
+        }
     }
 }
--- src/Gateways/Desktop/Views/Content/Rankings/CompetitionListView.xaml.cs
+++ src/Gateways/Desktop/Views/Content/Rankings/CompetitionListView.xaml.cs
@@ -1,15 +1,28 @@
 ﻿using EnduranceJudge.Gateways.Desktop.Core;
+using EnduranceJudge.Gateways.Desktop.Core.Services;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace EnduranceJudge.Gateways.Desktop.Views.Content.Rankings
 {
     public partial class CompetitionListView : UserControl, IView
     {
+        private readonly IInputHandler inputHandler;
+        public CompetitionListView(IInputHandler inputHandler) : this()
+        {
+            this.inputHandler = inputHandler;
+            this.inputHandler = inputHandler;
+        }
         public CompetitionListView()
         {
             InitializeComponent();
         }
 
         public string RegionName => Regions.CONTENT_RIGHT;
+
+        public void HandleScroll(object sender, MouseWheelEventArgs args)
+        {
+            this.inputHandler.HandleScroll(sender, args);
+        }
     }
 }
