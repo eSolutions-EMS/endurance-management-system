5d61ba4bc4d768e7a1bd25b89f77ae200c5f829d
Alexander Chobanov <alexander.chobanov@dowjones.com>
2022-02-04 16:31:47 +0200

    Remove unused code from ViewModelBase

--- src/Gateways/Desktop/Core/ViewModelBase.cs
+++ src/Gateways/Desktop/Core/ViewModelBase.cs
@@ -1,51 +1,34 @@
-﻿using EnduranceJudge.Core.Utilities;
-using EnduranceJudge.Gateways.Desktop.Core.Objects;
-using Prism.Commands;
-using Prism.Events;
+﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
 
-namespace EnduranceJudge.Gateways.Desktop.Core
+namespace EnduranceJudge.Gateways.Desktop.Core;
+
+public abstract class ViewModelBase : BindableBase, INavigationAware
 {
-    public abstract class ViewModelBase : BindableBase, INavigationAware
+    protected IRegionNavigationJournal Journal { get; set; }
+
+    public DelegateCommand NavigateForward => new (this.NavigateForwardAction);
+    public DelegateCommand NavigateBack => new (this.NavigateBackAction);
+
+    public virtual void OnNavigatedTo(NavigationContext context)
     {
-        protected ViewModelBase()
-        {
-            this.EventAggregator = StaticProvider.GetService<IEventAggregator>();
-        }
+        this.Journal = context.NavigationService.Journal;
+    }
+    public virtual bool IsNavigationTarget(NavigationContext context)
+    {
+        return false;
+    }
+    public virtual void OnNavigatedFrom(NavigationContext navigationContext)
+    {
+    }
 
-        protected IEventAggregator EventAggregator { get; }
-        protected IRegionNavigationJournal Journal { get; set; }
-
-        public DelegateCommand NavigateForward => new (this.NavigateForwardAction);
-        public DelegateCommand NavigateBack => new (this.NavigateBackAction);
-
-        public virtual void OnNavigatedTo(NavigationContext context)
-        {
-            this.Journal = context.NavigationService.Journal;
-        }
-        public virtual bool IsNavigationTarget(NavigationContext context)
-        {
-            return false;
-        }
-        public virtual void OnNavigatedFrom(NavigationContext navigationContext)
-        {
-        }
-
-        protected virtual void NavigateForwardAction()
-        {
-            this.Journal.GoForward();
-        }
-        protected virtual void NavigateBackAction()
-        {
-            this.Journal.GoBack();
-        }
-
-        protected virtual void ValidationError(string message)
-        {
-            this.EventAggregator
-                .GetEvent<ErrorEvent>()
-                .Publish(message);
-        }
+    protected virtual void NavigateForwardAction()
+    {
+        this.Journal.GoForward();
+    }
+    protected virtual void NavigateBackAction()
+    {
+        this.Journal.GoBack();
     }
 }
