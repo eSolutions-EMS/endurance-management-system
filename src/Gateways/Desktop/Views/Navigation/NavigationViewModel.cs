12665dba17a82729f52ea75e95998250228c895c
Alexander Chobanov <alexander.chobanov@dowjones.com>
2022-02-04 17:34:29 +0200

    Remove PrintExample setup

--- src/Gateways/Desktop/Views/Navigation/NavigationViewModel.cs
+++ src/Gateways/Desktop/Views/Navigation/NavigationViewModel.cs
@@ -1,27 +1,21 @@
 ﻿using EnduranceJudge.Gateways.Desktop.Core;
 using EnduranceJudge.Gateways.Desktop.Services;
-using EnduranceJudge.Gateways.Desktop.Views.Content.PrintExample;
 using Prism.Commands;
 
-namespace EnduranceJudge.Gateways.Desktop.Views.Navigation
+namespace EnduranceJudge.Gateways.Desktop.Views.Navigation;
+
+public class NavigationViewModel : ViewModelBase
 {
-    public class NavigationViewModel : ViewModelBase
+    public NavigationViewModel(INavigationService navigation)
     {
-        public NavigationViewModel(INavigationService navigation)
-        {
-            this.NavigateToImport = new DelegateCommand(navigation.NavigateToImport);
-            this.NavigateToEvent = new DelegateCommand(navigation.NavigateToEvent);
-            this.NavigateToManager = new DelegateCommand(navigation.NavigateToManager);
-            this.NavigateToRanking = new DelegateCommand(navigation.NavigateToRanking);
-
-            this.NavigateToPrintExample = new DelegateCommand(navigation.ChangeTo<PrintExampleView>);
-        }
-
-        public DelegateCommand NavigateToImport { get; }
-        public DelegateCommand NavigateToEvent { get; }
-        public DelegateCommand NavigateToManager { get; }
-        public DelegateCommand NavigateToPrintExample { get; }
-        public DelegateCommand NavigateToRanking { get; }
-
+        this.NavigateToImport = new DelegateCommand(navigation.NavigateToImport);
+        this.NavigateToEvent = new DelegateCommand(navigation.NavigateToEvent);
+        this.NavigateToManager = new DelegateCommand(navigation.NavigateToManager);
+        this.NavigateToRanking = new DelegateCommand(navigation.NavigateToRanking);
     }
+
+    public DelegateCommand NavigateToImport { get; }
+    public DelegateCommand NavigateToEvent { get; }
+    public DelegateCommand NavigateToManager { get; }
+    public DelegateCommand NavigateToRanking { get; }
 }
