53bea729ed562a98439c2f6ab5487a554e8524b5
Alexander Chobanov <alexander.chobanov@dowjones.com>
2022-02-04 17:11:40 +0200

    Register class handler for KeyboardFocustChange event

--- src/Gateways/Desktop/Core/Services/IInputHandler.cs
+++ src/Gateways/Desktop/Core/Services/IInputHandler.cs
@@ -1,4 +1,5 @@
 ﻿using EnduranceJudge.Core.ConventionalServices;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -6,11 +7,32 @@ namespace EnduranceJudge.Gateways.Desktop.Core.Services;
 
 public class InputHandler : IInputHandler
 {
+    public InputHandler()
+    {
+        EventManager.RegisterClassHandler(
+            typeof(TextBox),
+            UIElement.GotKeyboardFocusEvent,
+            new KeyboardFocusChangedEventHandler(this.HandleKeyboardFocus));
+    }
+
     public void HandleScroll(object sender, MouseWheelEventArgs scrollEvent)
     {
         var scv = (ScrollViewer)sender;
         scv.ScrollToVerticalOffset(scv.VerticalOffset - scrollEvent.Delta);
         scrollEvent.Handled = true;
+    }
+
+    private void HandleKeyboardFocus(object sender, KeyboardFocusChangedEventArgs args)
+    {
+        var prev = args.OldFocus;
+        if (prev is TextBox prevTextBox)
+        {
+            prevTextBox.Select(0, 0);
+        }
+        if (sender is TextBox {IsReadOnly: false} textBox && args.KeyboardDevice.IsKeyDown(Key.Tab))
+        {
+            textBox.SelectAll();
+        }
     }
 }
 
