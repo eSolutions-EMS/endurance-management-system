225360ace87aee5d11737a381aeffa793f2a4511
Alexander Chobanov <alexander.chobanov@dowjones.com>
2022-03-05 19:20:54 +0200

    Remove unused file

--- src/Domain/AggregateRoots/Import/ImportObjectException.cs
+++
@@ -1,9 +0,0 @@
-﻿using EnduranceJudge.Domain.Core.Exceptions;
-
-namespace EnduranceJudge.Domain.AggregateRoots.Import
-{
-    public class ImportObjectException : DomainExceptionBase
-    {
-        protected override string Entity { get; } = nameof(ImportRoot);
-    }
-}
