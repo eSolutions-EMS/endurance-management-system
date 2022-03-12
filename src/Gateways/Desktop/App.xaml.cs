2844c9512c19fe810d9d6108662551349dafd0ef
Alexander Chobanov <alexander.chobanov@dowjones.com>
2022-03-12 18:47:39 +0200

    Remove test method from App

--- src/Gateways/Desktop/App.xaml.cs
+++ src/Gateways/Desktop/App.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using EnduranceJudge.Core.Services;
 using EnduranceJudge.Core.Utilities;
-using EnduranceJudge.Domain.State.Athletes;
-using EnduranceJudge.Domain.State.Horses;
-using EnduranceJudge.Domain.State.Competitions;
-using EnduranceJudge.Domain.State.Participants;
-using EnduranceJudge.Domain.State.Personnels;
-using EnduranceJudge.Domain.AggregateRoots.Common.Performances;
-using EnduranceJudge.Domain.State.Laps;
-using EnduranceJudge.Domain.Enums;
-using EnduranceJudge.Domain.State.EnduranceEvents;
 using EnduranceJudge.Gateways.Desktop.Startup;
 using EnduranceJudge.Gateways.Desktop.Views;
 using EnduranceJudge.Gateways.Desktop.Core;
-using EnduranceJudge.Gateways.Desktop.Core.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Prism.DryIoc;
 using Prism.Ioc;
@@ -52,7 +42,6 @@ namespace EnduranceJudge.Gateways.Desktop
         {
             var aspNetProvider = this.Container.Resolve<IServiceProvider>();
             InitializeStaticServices(aspNetProvider);
-            // this.BigDick();
 
             var initializers = aspNetProvider.GetServices<IInitializer>();
             foreach (var initializer in initializers.OrderBy(x => x.RunningOrder))
@@ -86,56 +75,5 @@ namespace EnduranceJudge.Gateways.Desktop
         {
             StaticProvider.Initialize(provider);
         }
-
-        // private void BigDick()
-        // {
-        //     var serializer = ServiceProvider.GetService<IJsonSerializationService>();
-        //
-        //     var eventState = new EnduranceEvent("name", "place", "BUL");
-        //     var president = new Personnel("Pesho Goshov", PersonnelRole.PresidentGroundJury);
-        //     var steward1 = new Personnel("Stew Stew", PersonnelRole.Steward);
-        //     var steward2 = new Personnel("Two two", PersonnelRole.Steward);
-        //     eventState.Add(president);
-        //     eventState.Add(steward1);
-        //     eventState.Add(steward2);
-        //     var horse1 = new Horse("feiId", "name", "breed", "club");
-        //     var horse2 = new Horse("feiId2", "name2", "breed", "club");
-        //     var athlete1 = new Athlete("feiId", "name1", "name2", "country", Category.Kids);
-        //     var athlete2 = new Athlete("feiId", "name1", "name2", "country", Category.Adults);
-        //     var participant1 = new Participant(horse1, athlete1, 10, "rfId", 16);
-        //     var participant2 = new Participant(horse2, athlete2, 11, "rfId", 16);
-        //     // eventState.Add(participant1);
-        //     // eventState.Add(participant2);
-        //
-        //     var competition1 = new Competition(CompetitionType.National, "Name", DateTime.Now.AddDays(1));
-        //     eventState.Add(competition1);
-        //     participant1.ParticipateIn(competition1);
-        //     participant2.ParticipateIn(competition1);
-        //
-        //     var lap1 = new Lap(false, 10, 15, 10, 20);
-        //     competition1.Add(lap1);
-        //     var lapEntry1 = new LapEntry(lap1, competition1.StartTime);
-        //     var lapEntry2 = new LapEntry(lap1, competition1.StartTime);
-        //     participant1.Participation.LapEntries.Add(lapEntry1);
-        //     participant2.Participation.LapEntries.Add(lapEntry2);
-        //
-        //     var lap2 = new Lap(true, 20, 30, 10, 20);
-        //     competition1.Add(lap2);
-        //
-        //     var competition2 = new Competition(CompetitionType.International, "Name", DateTime.Now.AddDays(1));
-        //     eventState.Add(competition2);
-        //     participant1.ParticipateIn(competition2);
-        //
-        //     var lap3 = new Lap(false, 10, 15, 10, 20);
-        //     competition2.Add(lap3);
-        //     var lapEntry3 = new LapEntry(lap1, competition1.StartTime);
-        //     participant1.Participation.LapEntries.Add(lapEntry3);
-        //
-        //     var lap4 = new Lap(true, 20, 30, 10, 20);
-        //     competition2.Add(lap4);
-        //
-        //     var json = serializer.Serialize(eventState);
-        //     var result = serializer.Deserialize<EnduranceEvent>(json);
-        // }
     }
 }
