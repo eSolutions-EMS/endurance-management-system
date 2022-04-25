b062df38460cc8965c911311d29db7183ef53bcc
Alexander Chobanov <alexander.chobanov@dowjones.com>
2022-04-25 18:05:29 +0300

    Add Scale helper method

--- src/Gateways/Desktop/Controls/ControlsHelper.cs
+++ src/Gateways/Desktop/Controls/ControlsHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace EnduranceJudge.Gateways.Desktop.Controls;
 
@@ -6,4 +7,44 @@ public class ControlsHelper
 {
     public static Style GetStyle(string key)
         => (Style) System.Windows.Application.Current.FindResource(key);
+
+    public static void Scale(UIElement element, double coefficient)
+        => ScaleElementTree(element, coefficient);
+
+    /// <summary>
+    /// Limited functionality.
+    /// </summary>
+    /// <param name="element"></param>
+    /// <param name="coefficient"></param>
+    private static void ScaleElementTree(UIElement element, double coefficient)
+    {
+        ScaleElement(element, coefficient);
+        if (element is Decorator decorator)
+        {
+            ScaleElementTree(decorator.Child, coefficient);
+        }
+        else if (element is Panel panel)
+        {
+            foreach (var child in panel.Children)
+            {
+                if (child is UIElement uiElement)
+                {
+                    ScaleElementTree(uiElement, coefficient);
+                }
+            }
+        }
+    }
+
+    private static void ScaleElement(UIElement element, double coefficient)
+    {
+        if (element is Border border)
+        {
+            border.Height = border.ActualHeight * coefficient;
+            border.Width = border.ActualWidth * coefficient;
+        }
+        else if (element is TextBlock textBlock)
+        {
+            textBlock.FontSize *= coefficient;
+        }
+    }
 }
