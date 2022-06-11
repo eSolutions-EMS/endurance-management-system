4715ae5efc0528db331188fc06f74ce0e17546b0
Alexander Chobanov <alexander.chobanov@dowjones.com>
2022-06-11 13:40:00 +0300

    Remove tools dir

--- tools/solution/Console/PrintCountries/CountryModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EnduranceJudge.Tools.Console.PrintCountries
-{
-    public class CountryModel
-    {
-        public string Code { get; set; }
-        public string Name { get; set; }
-    }
-}
