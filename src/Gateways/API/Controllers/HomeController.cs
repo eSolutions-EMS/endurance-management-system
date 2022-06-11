296a16e056c76a85e33d28c2545699c22d21931f
Alexander Chobanov <alexander.chobanov@dowjones.com>
2022-06-11 13:44:15 +0300

    Create API project

---
+++ src/Gateways/API/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using API.Requests;
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromBody] TagRequest tag)
+        {
+            Console.WriteLine($"Received: {tag.Id}");
+            return this.Ok();
+        }
+    }
+}
