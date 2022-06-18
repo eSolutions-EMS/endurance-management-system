bb6a5c6cde5d114c85b137f8484b796d470a39bc
Alexander Chobanov <alexander.chobanov@dowjones.com>
2022-06-18 16:39:48 +0300

    Add StateController
    This endpoint is exposed to Judge
    to read and write data

---
+++ src/Gateways/API/Controllers/StateController.cs
@@ -0,0 +1,33 @@
+﻿using Endurance.Judge.Gateways.API.Requests;
+using Endurance.Judge.Gateways.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Endurance.Judge.Gateways.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StateController : ControllerBase
+    {
+        private readonly IContext context;
+        private readonly IStateChangesQueue stateChangesQueue;
+        public StateController(IContext context, IStateChangesQueue stateChangesQueue)
+        {
+            this.context = context;
+            this.stateChangesQueue = stateChangesQueue;
+        }
+        
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var raw = this.context.State;
+            return this.Ok(raw);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] StateRequest request)
+        {
+            this.stateChangesQueue.Enqueue(request.State);
+            return this.Ok();
+        }
+    }
+}
---
+++ src/Gateways/API/Services/Context.cs
@@ -0,0 +1,15 @@
+﻿using EnduranceJudge.Application;
+using EnduranceJudge.Core.ConventionalServices;
+
+namespace Endurance.Judge.Gateways.API.Services
+{
+    public class Context : IContext
+    {
+        public State State { get; set; }
+    }
+
+    public interface IContext : ISingletonService
+    {
+        State State { get; }
+    }
+}
