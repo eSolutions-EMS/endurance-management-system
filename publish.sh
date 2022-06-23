f05154a91a2e82fdade4eea04fe878bd8b9f5867
Alexander Chobanov <alexander.chobanov@dowjones.com>
2022-06-23 17:31:47 +0300

    Improve publish.sh

--- publish.sh
+++ publish.sh
@@ -1,16 +1,21 @@
+cd release
 echo clearing release directory...
-rm -rf ./release/*
+rm -rf *
 echo release directory cleared.
 echo =======================================================================================================================================
 echo DOTNET PUBLISH
-dotnet publish -c Release -o ./release src
+dotnet publish -c Release -o . ../src
 echo =======================================================================================================================================
 echo copying Views...
-mkdir ./release/Views
-mkdir ./release/Views/Templates
-cp -r src/Gateways/Desktop/Views/Templates/* ./release/Views/Templates
+mkdir ./Views
+mkdir ./Views/Templates
+cp -r ../src/Gateways/Desktop/Views/Templates/* ./Views/Templates
 echo Views directory copied.
 echo =======================================================================================================================================
 echo opening explorer...
+mkdir logs
+touch logs/witness-events.log
+touch logs/witness-event-errors.log
+cd ..
 explorer release
 echo publish complete.
