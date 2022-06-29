305d7a0dc02952262b7b89465a4a39e7815a4808
Alexander Chobanov <alexander.chobanov@dowjones.com>
2022-06-29 16:42:37 +0300

    Revert "test"
    This reverts commit e24a9c2de26f2c2f0f3fabd40c1aa7d4eeb3d28b.

--- src/Application/Services/WitnessPollingService.cs
+++ src/Application/Services/WitnessPollingService.cs
@@ -23,7 +23,7 @@ public class WitnessPollingService : IWitnessPollingService
         this.logger = logger;
         this.dataService = dataService;
         this.witnessEventQueue = witnessEventQueue;
-        this.persistence = persistence; // test
+        this.persistence = persistence;
     }
 
     public void ApplyEvents()
