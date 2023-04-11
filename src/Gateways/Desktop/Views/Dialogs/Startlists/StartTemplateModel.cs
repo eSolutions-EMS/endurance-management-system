10568ba2ab36ae2ab2b6737789f407ff467d09c2
Alexander Chobanov <alexander.chobanov@dowjones.com>
2023-04-11 14:32:40 +0300

    Remove unnecessary fields from Startlist

--- src/Gateways/Desktop/Views/Dialogs/Startlists/StartTemplateModel.cs
+++ src/Gateways/Desktop/Views/Dialogs/Startlists/StartTemplateModel.cs
@@ -7,20 +7,14 @@ public class StartTemplateModel
 {
     public StartTemplateModel(StartModel start)
     {
-        this.Number = start.Number;
         this.Name = start.Name;
         this.CountryName = start.CountryName;
         this.Distance = start.Distance;
-        this.HasStarted = start.HasStarted
-            ? YES
-            : NO;
         this.StartTimeString = start.StartTime.ToString(DesktopConstants.TIME_FORMAT);
     }
 
-    public string Number { get; }
     public string Name { get; }
     public string CountryName { get; }
     public double Distance { get; }
     public string StartTimeString { get; }
-    public string HasStarted { get; }
 }
