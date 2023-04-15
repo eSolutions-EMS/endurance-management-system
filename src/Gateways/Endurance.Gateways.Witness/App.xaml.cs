0b35ca7737584fbb1459e0503afee2adabbb0de2
achobanov <alexchobanov@gmail.com>
2023-04-15 23:19:26 +0300

    Create Witness MAUI app

---
+++ src/Gateways/Endurance.Gateways.Witness/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Endurance.Gateways.Witness;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ src/Gateways/Endurance.Gateways.Witness/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Endurance.Gateways.Witness;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
