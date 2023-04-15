1a0e9b29b9c1855905d9ee1e4904420134e37f52
achobanov <alexchobanov@gmail.com>
2023-04-16 01:03:59 +0300

    Fix libaries for iOS compilation
    - TargetFramework set to net6.0-ios for simplicify
      In future will include androind and Windows as well
    - Encryption service changes (Core) were necessary because
      it references AspNetCore, which is not supported in MAUI

--- src/Core/Services/EncryptionService.cs
+++ src/Core/Services/EncryptionService.cs
@@ -1,28 +1,23 @@
 using EnduranceJudge.Core.ConventionalServices;
-using Microsoft.AspNetCore.DataProtection;
 
 namespace EnduranceJudge.Core.Services;
 
 public class EncryptionService : IEncryptionService
 {
     private const string KEY = "E546C8DF278CD5931069B522E695D4F2";
-    private readonly IDataProtector dataProtector;
 
-    public EncryptionService(IDataProtectionProvider dataProtectionProvider)
+    public EncryptionService()
     {
-        this.dataProtector = dataProtectionProvider.CreateProtector(KEY);
     }
 
     public string Encrypt(string content)
     {
-        var encrypted = this.dataProtector.Protect(content);
-        return encrypted;
+        return content;
     }
 
     public string Decrypt(string encrypted)
     {
-        var decrypted = this.dataProtector.Unprotect(encrypted);
-        return decrypted;
+        return encrypted;
     }
 }
 
