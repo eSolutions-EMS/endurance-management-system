0c40ec422ad82033afc9422d0fcde5f56fb3e56f
achobanov <alexchobanov@gmail.com>
2023-04-16 14:22:08 +0300

    Create WitnessPage

--- src/Gateways/Endurance.Gateways.Witness/MauiProgram.cs
+++ src/Gateways/Endurance.Gateways.Witness/MauiProgram.cs
@@ -19,7 +19,7 @@ public static class MauiProgram
 #if DEBUG
 		builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
-		
+
 		builder.Services.AddSingleton<WeatherForecastService>();
 
 		return builder.Build();
---
+++ src/Gateways/Endurance.Gateways.Witness/Pages/WitnessPage.razor.cs
@@ -0,0 +1,50 @@
+﻿using EnduranceJudge.Domain.AggregateRoots.Manager;
+using Microsoft.AspNetCore.Components;
+
+namespace Endurance.Gateways.Witness.Pages;
+public partial class WitnessPage : ComponentBase
+{
+    private Model witnessModel = new();
+    private Dictionary<int, ManualWitnessEvent> recordedEvents = new();
+
+    private void HandleSubmit()
+    {
+        var witnessEvent = new ManualWitnessEvent
+        {
+            Number = this.witnessModel.Number.Value,
+            Time = DateTime.Today
+                .AddHours(this.witnessModel.Hour.Value)
+                .AddMinutes(this.witnessModel.Minute.Value)
+                .AddSeconds(this.witnessModel.Second.Value),
+            Type = Enum.Parse<WitnessEventType>(this.witnessModel.Type),
+        };
+        if (this.recordedEvents.ContainsKey(witnessEvent.Number))
+        {
+            this.recordedEvents.Remove(witnessEvent.Number);
+        }
+        this.recordedEvents.Add(witnessEvent.Number, witnessEvent);
+    }
+
+    private async Task Save(int number)
+    {
+        var witnessEvent = this.recordedEvents[number];
+        // send to API over HTTP
+        var result = new Random().Next(2);
+        if (result == 0)
+        {
+            return;
+        }
+        this.recordedEvents.Remove(number);           
+    }
+
+    private class Model
+    {
+        public string Type = WitnessEventType.Arrival.ToString();
+        public int? Number;
+        public int? Hour;
+        public int? Minute;
+        public int? Second;
+    }
+}
+
+
