2b60a02c3d76186f706c4ed75528e319f445c6a9
achobanov <alexchobanov@gmail.com>
2023-04-16 09:39:42 +0300

    Create StartlistPage

--- src/Domain/AggregateRoots/Manager/Aggregates/Startlists/StartModel.cs
+++ src/Domain/AggregateRoots/Manager/Aggregates/Startlists/StartModel.cs
@@ -4,10 +4,11 @@ namespace EnduranceJudge.Domain.AggregateRoots.Manager.Aggregates.Startlists;
 
 public class StartModel
 {
-    public string Number { get; internal init; }
-    public string Name { get; internal init; }
-    public string CountryName { get; internal init; }
-    public double Distance { get; internal init; }
-    public DateTime StartTime { get; internal init; }
-    public bool HasStarted { get; internal init; }
+    //TODO: Revert to internal set after AIP integration of StartListPage
+    public string Number { get; init; }
+    public string Name { get; init; }
+    public string CountryName { get; init; }
+    public double Distance { get; init; }
+    public DateTime StartTime { get; init; }
+    public bool HasStarted { get; init; }
 }
