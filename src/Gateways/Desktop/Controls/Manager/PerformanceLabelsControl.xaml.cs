1c9b60f815346e4fad39d0093f54e52118724de8
achobanov <alexchobanov@gmail.com>
2023-04-30 16:19:14 +0300

    Shrink Performance Table in Manager and Ranking

--- src/Gateways/Desktop/Controls/Manager/PerformanceLabelsControl.xaml.cs
+++ src/Gateways/Desktop/Controls/Manager/PerformanceLabelsControl.xaml.cs
@@ -1,7 +1,4 @@
-﻿using EnduranceJudge.Domain.State.Participations;
-using System.Windows;
-
-namespace EnduranceJudge.Gateways.Desktop.Controls.Manager;
+﻿namespace EnduranceJudge.Gateways.Desktop.Controls.Manager;
 
 public partial class PerformanceLabelsControl
 {
@@ -9,17 +6,4 @@ public partial class PerformanceLabelsControl
     {
         InitializeComponent();
     }
-
-    public string Number
-    {
-        get => $"#{this.NumberText}";
-        set => this.NumberText.Text = $"#{value}";
-    }
-
-    public static readonly DependencyProperty NUMBER_PROPERTY =
-        DependencyProperty.Register(
-            nameof(Number),
-            typeof(string),
-            typeof(PerformanceLabelsControl),
-            new PropertyMetadata(null));
 }
