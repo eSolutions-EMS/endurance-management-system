63f5e683212c7c09f538178f4cb22df6155920d9
achobanov <alexchobanov@gmail.com>
2023-05-06 16:08:41 +0300

    Add time formatting in StartList page

--- src/Application/Services/DateService.cs
+++ src/Application/Services/DateService.cs
@@ -4,16 +4,18 @@ using System;
 namespace EnduranceJudge.Application.Services;
 public class DateService : IDateService
 {
-    public string FormatTime(DateTime time)
-        => time.ToString("HH:mm:ss.fff");
+    public string FormatTime(DateTime time, bool showMs = false)
+        => showMs
+            ? time.ToString("HH:mm:ss.fff")
+            : time.ToString("HH:mm:ss");
 
-    public DateTime Now()
+	public DateTime Now()
         => DateTime.Now;  
 }
 
 public interface IDateService : ITransientService
 {
     DateTime Now();
-    string FormatTime(DateTime time);
+    string FormatTime(DateTime time, bool showMs = false);
 
 }
--- src/Domain/AggregateRoots/Manager/Aggregates/Startlists/StartModel.cs
+++ src/Domain/AggregateRoots/Manager/Aggregates/Startlists/StartModel.cs
@@ -4,11 +4,10 @@ namespace EnduranceJudge.Domain.AggregateRoots.Manager.Aggregates.Startlists;
 
 public class StartModel
 {
-    //TODO: Revert to internal set after AIP integration of StartListPage
-    public string Number { get; init; }
-    public string Name { get; init; }
-    public string CountryName { get; init; }
-    public double Distance { get; init; }
-    public DateTime StartTime { get; init; }
-    public bool HasStarted { get; init; }
+    public string Number { get; internal set; }
+    public string Name { get; internal set; }
+    public string CountryName { get; internal set; }
+    public double Distance { get; internal set; }
+    public DateTime StartTime { get; internal set; }
+    public bool HasStarted { get; internal set; }
 }
