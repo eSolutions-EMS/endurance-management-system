2a26922fccb4f711d8e2bdc40ec5b8a3cf8bafe2
achobanov <alexchobanov@gmail.com>
2023-06-04 19:21:43 +0300

    Attach Arrivelist client events in App.xaml.cs
    Because ArrivelistService might not be constructed when the client
    starts it's constructor should not be used to subscribe to events.
    Instead subscriptions for now will happen in the App class
    
    https://github.com/eSolutions-EMS/endurance-management-system/issues/138

--- src/Witness/EMS.Witness/App.xaml.cs
+++ src/Witness/EMS.Witness/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Application.Rpc;
 using Core.Events;
+using EMS.Witness.Rpc;
 using EMS.Witness.Services;
 using EMS.Witness.Shared.Toasts;
 
@@ -8,29 +9,54 @@ namespace EMS.Witness;
 public partial class App : Application
 {
     private readonly ToasterService toaster;
-    private readonly IEnumerable<IRpcClient> rpcClients;
+    private readonly IRpcService rpcService;
+    private readonly IArrivelistClient arrivelistClient;
+    private readonly IArrivelistService arrivelistService;
 
-    public App(ToasterService toaster, IEnumerable<IRpcClient> rpcClients)
+    public App(
+		ToasterService toaster,
+		IEnumerable<IRpcClient> rpcClients,
+		IRpcService rpcService,
+		IArrivelistClient arrivelistClient,
+		IArrivelistService arrivelistService)
 	{
 		this.InitializeComponent();
         this.MainPage = new MainPage();
 
         this.toaster = toaster;
-        this.rpcClients = rpcClients;
-
-		this.HandleRpcErrors();
+        this.rpcService = rpcService;
+        this.arrivelistClient = arrivelistClient;
+        this.arrivelistService = arrivelistService;
+        this.HandleRpcErrors(rpcClients);
 		this.HandleCoreErrors();
+		this.AttachEventHandlers();
     }
 
 	protected override Window CreateWindow(IActivationState? activationState)
 	{
 		var window = base.CreateWindow(activationState);
+
+		window.Activated += (s, e) => this.rpcService.Handshake();
+
 		return window;
 	}
 
-	private void HandleRpcErrors()
+	private void AttachEventHandlers()
 	{
-		foreach (var client in this.rpcClients)
+
+        this.arrivelistClient.Updated += (sender, args) => this.arrivelistService.Update(args.entry, args.action);
+        this.arrivelistClient.ServerConnectionChanged += async (sender, isConnected) => 
+		{
+			if (isConnected)
+			{
+				await this.arrivelistService.Load();
+			}
+        };
+    }
+
+	private void HandleRpcErrors(IEnumerable<IRpcClient> rpcClients)
+	{
+		foreach (var client in rpcClients)
 		{
             client.Error += (sender, error) =>
             {
