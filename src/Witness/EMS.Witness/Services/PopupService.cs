e12de91353f9e17b8e8d31061abfc983f280dc01
achobanov <alexchobanov@gmail.com>
2023-07-15 21:39:34 +0300

    Create PopupService to select EventType
    Unified method to select EventType when sending data to Judge
    Used in SaveFormComponent and HistoryComponent
    
    https://github.com/eSolutions-EMS/endurance-management-system/issues/150

---
+++ src/Witness/EMS.Witness/Services/PopupService.cs
@@ -0,0 +1,33 @@
+﻿using Core.ConventionalServices;
+using Core.Domain.AggregateRoots.Manager;
+
+namespace EMS.Witness.Services;
+
+public class PopupService : IPopupService
+{
+    private const string CLOSE = "Close";
+
+    public async Task<string> RenderActionSheet(string text, params string[] values)
+    {
+        return await Application.Current.MainPage.DisplayActionSheet("Select Type", CLOSE, null, values);
+    }
+
+    public async Task<WitnessEventType?> SelecEventType()
+    {
+        var result = await this.RenderActionSheet(
+            "Select Type",
+            WitnessEventType.Arrival.ToString(),
+            WitnessEventType.VetIn.ToString());
+        if (result == CLOSE)
+        {
+            return null;
+        }
+        return Enum.Parse<WitnessEventType>(result);
+    }
+}
+
+public interface IPopupService : ITransientService
+{
+    Task<WitnessEventType?> SelecEventType();
+    Task<string> RenderActionSheet(string text, params string[] values);
+}
