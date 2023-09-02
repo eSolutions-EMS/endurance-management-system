4a72ca7566d2beeb3cd5c5ed1122457041078c24
achobanov <alexchobanov@gmail.com>
2023-09-02 18:56:16 +0300

    Startlist uses live timers instead of timestamps
    https://github.com/eSolutions-EMS/endurance-management-system/issues/161

--- src/Core.Application/Services/DateService.cs
+++ src/Core.Application/Services/DateService.cs
@@ -10,6 +10,11 @@ public class DateService : IDateService
             ? time.ToString("HH:mm:ss.fff")
             : time.ToString("HH:mm:ss");
 
+	public string FormatTime(TimeSpan time, bool showMs = false)
+		=> showMs
+			? time.ToString(@"hh\:mm\:ss\.fff")
+            : time.ToString(@"hh\:mm\:ss");
+
 	public DateTime Now()
         => DateTime.Now;  
 }
@@ -18,5 +23,6 @@ public interface IDateService : ITransientService
 {
     DateTime Now();
     string FormatTime(DateTime time, bool showMs = false);
+    string FormatTime(TimeSpan time, bool showMs = false);
 
 }
