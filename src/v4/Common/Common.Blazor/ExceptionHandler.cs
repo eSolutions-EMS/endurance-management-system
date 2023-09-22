587c0d675c0486b3d84e8518ea3f29b9ef547015
achobanov <alexchobanov@gmail.com>
2023-09-22 14:24:38 +0300

    Create ExceptionsHandler component
    https://github.com/eSolutions-EMS/endurance-management-system/issues/164

---
+++ src/v4/Common/Common.Blazor/ExceptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using Common.Domain;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.Localization;
+
+namespace Common.Blazor;
+
+public class ExceptionHandler : ErrorBoundary
+{
+    [Inject]
+    public IStringLocalizer StringLocalizer { get; set; } = default!;
+
+    public string? LocalizedMessage
+    {
+        get
+        {
+            if (this.CurrentException == null)
+            {
+                return null;
+            }
+            if (this.CurrentException is DomainException domainException && domainException.Args.Any())
+            {
+                var localizedArguments = domainException.Args.Select(x => this.StringLocalizer[x]);
+                return this.StringLocalizer[domainException.Message, domainException.Args];
+            }
+            return this.StringLocalizer[this.CurrentException.Message];
+        }
+    }
+    public string? StackTrace
+        => this.CurrentException?.StackTrace;
+
+}
