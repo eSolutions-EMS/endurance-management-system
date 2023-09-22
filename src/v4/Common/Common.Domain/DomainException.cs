5a3ce4f657668beb9d11aea5afb4e6b21d5b47f5
achobanov <alexchobanov@gmail.com>
2023-09-22 14:25:22 +0300

    Create DomainException
    https://github.com/eSolutions-EMS/endurance-management-system/issues/164

---
+++ src/v4/Common/Common.Domain/DomainException.cs
@@ -0,0 +1,17 @@
+﻿using System.Dynamic;
+
+namespace Common.Domain;
+
+public class DomainException : Exception
+{
+    public DomainException(string message) : base(message)
+    {
+    }
+
+    public DomainException(string message, params string[] args) : base(message)
+    {
+        this.Args = args;
+    }
+
+    public string[] Args { get; } = Array.Empty<string>();
+}
