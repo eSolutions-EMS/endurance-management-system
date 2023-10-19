5ae9dddcb3cae3da435162d6a0113fb5b6c248e4
achobanov <alexchobanov@gmail.com>
2023-10-19 18:01:13 +0300

    Log disconnect time and reconnect time in VF747pController

--- src/Judge/EMS.Judge.Application/Hardware/RfidController.cs
+++ src/Judge/EMS.Judge.Application/Hardware/RfidController.cs
@@ -30,7 +30,6 @@ public abstract class RfidController
     {
         var message = $"{this.Device} ERROR: {error}";
         Console.WriteLine($"{DateTime.Now}: {message}");
-        //this.ErrorEvent?.Invoke(this, message);
     }
 
     public abstract void Connect();
