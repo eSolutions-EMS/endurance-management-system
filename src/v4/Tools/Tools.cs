8d092d9cd1dbb8e9214394094e32aa3965bdd25b
achobanov <alexchobanov@gmail.com>
2023-10-30 23:59:02 +0200

    Add Tools console project
    https://github.com/eSolutions-EMS/endurance-management-system/issues/171

---
+++ src/v4/Tools/Tools.cs
@@ -0,0 +1,15 @@
+﻿
+using Common.Conventions;
+using Common.Domain.Ports;
+using EMS.Domain.Setup.Entities;
+using EMS.Judge.Setup.Events;
+using EMS.Persistence.Startup;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+services.AddPersistence();
+services.GetConventionalAssemblies().RegisterConventionalServices();
+
+var provider = services.BuildServiceProvider();
+var eventService = provider.GetRequiredService<IUpdateEvent>();
+;
