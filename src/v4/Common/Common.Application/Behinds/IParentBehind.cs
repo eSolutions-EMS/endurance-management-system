61412330151665939911d1de70bff392824b55d1
achobanov <alexchobanov@gmail.com>
2024-02-11 20:17:25 +0200

    Create IBehind interface
    An IBehind implementation represents the entry point of application
    logic to be executed in components or API endpoints.
    
    https://github.com/eSolutions-EMS/endurance-management-system/issues/171

---
+++ src/v4/Common/Common.Application/Behinds/IParentBehind.cs
@@ -0,0 +1,18 @@
+﻿using Common.Conventions;
+using Common.Domain;
+
+namespace Common.Application.Behinds;
+
+/// <summary>
+/// <seealso cref="IParentBehind{T}"/> represents a <seealso cref="IBehind"/> that manages child <seealso cref="DomainEntity"/> objects
+/// (see <seealso cref="IParent{T}"/> for more). Details regaring it's parent entity are responsiblity of the implementation
+/// </summary>
+/// <typeparam name="T">Type of the child entity</typeparam>
+public interface IParentBehind<T> : ISingletonService
+    where T : DomainEntity
+{
+    IEnumerable<T> Children { get; }
+    Task<T> Create(T child);
+    Task<T> Delete(T child);
+    Task<T> Update(T child);
+}
--- src/v4/Judge/EMS.Judge/Events/EventBehind.cs
+++ src/v4/Judge/EMS.Judge/Events/EventBehind.cs
@@ -1,31 +1,45 @@
-﻿using Common.Application;
+﻿using Common.Application.Behinds;
 using Common.Application.CRUD;
 using Common.Application.CRUD.Parents;
-using Common.Domain;
 using Common.Helpers;
 using EMS.Domain.Setup.Entities;
 
 namespace EMS.Judge.Events;
 
-public class EventBehind : IParentBehind<Official>
+public class EventBehind : IBehind<Event>, IParentBehind<Official>
 {
     private readonly IRepository<Event> _repository;
     private readonly IParentRepository<Official> _officialRepository;
     private Event? _event;
 
-    public EventBehind(IRepository<Event> eventReader, IParentRepository<Official> officialRepository)
+    public EventBehind(IRepository<Event> events, IParentRepository<Official> officials)
     {
-        _repository = eventReader;
-        _officialRepository = officialRepository;
+        _repository = events;
+        _officialRepository = officials;
     }
 
-    public DomainEntity? Parent => _event;
     public IEnumerable<Official> Children => _event?.Officials ?? Enumerable.Empty<Official>();
 
-    public async Task Init(int parentId)
+    public async Task<Event?> Read(int id)
     {
         var events = await _repository.Read(x => true);
         _event = events.FirstOrDefault();
+        return _event;
+    }
+
+    public async Task<Event> Create(Event entity)
+    {
+        return await _repository.Create(entity);
+    }
+
+    public async Task<Event> Update(Event entity)
+    {
+        return await _repository.Update(entity);
+    }
+
+    public async Task<Event> Delete(int id)
+    {
+        return await _repository.Delete(id);
     }
 
     public async Task<Official> Create(Official child)
@@ -37,12 +51,12 @@ public class EventBehind : IParentBehind<Official>
         return child;
     }
 
-    public async Task Delete(Official child)
+    public async Task<Official> Delete(Official child)
     {
         ThrowHelper.ThrowIfNull(_event);
 
         _event.Remove(child);
-        await _officialRepository.Delete(_event.Id, child);
+        return await _officialRepository.Delete(_event.Id, child);
     }
 
     public async Task<Official> Update(Official child)
