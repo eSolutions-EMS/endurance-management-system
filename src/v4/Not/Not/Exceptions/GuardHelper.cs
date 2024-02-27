79c7aa409058b745dbfcd1a2f6a460788178dd1b
achobanov <alexchobanov@gmail.com>
2024-02-27 20:13:18 +0200

    Add docs

--- src/v4/Not/Not/Exceptions/GuardHelper.cs
+++ src/v4/Not/Not/Exceptions/GuardHelper.cs
@@ -2,8 +2,15 @@
 
 namespace Not.Exceptions;
 
+/// <summary>
+/// GuardHelper deals with exceptions that should never be seen by the end-user
+/// </summary>
 public static class GuardHelper
 {
+    /// <summary>
+    /// Mainly used in order to prevent nullable warnings
+    /// </summary>
+    /// <exception cref="GuardException"></exception>
     [DoesNotReturn]
     public static void ThrowIfNull(object? value, string message = "Object cannot be null")
     {
@@ -13,6 +20,7 @@ public static class GuardHelper
         }
     }
 
+    
     public static Exception Exception(string message)
     {
         return new GuardException(message);
