abafd2924641bb5f03689c9266eff86b5798c35a
Alexander Chobanov <alexander.chobanov@egt-digital.com>
2024-03-16 21:26:49 +0200

    Create LoggingClient in Witness
    https://github.com/Not-Endurance/not-timing-system/issues/179

---
+++ src/Witness/EMS.Witness/Rpc/LoggingClient.cs
@@ -0,0 +1,52 @@
+﻿using Core.Application.Rpc;
+using Core.Application.Rpc.Procedures;
+
+namespace EMS.Witness.Rpc;
+
+public class LoggingClient : RpcClient, IWitnessLogger
+{
+	public LoggingClient(RpcContext context) : base(context)
+	{
+	}
+
+	public void Log(string message)
+	{
+		var clientId = GetClientId();
+		var log = new RpcLog(clientId, message);
+		SendInBackground(log);
+	}
+
+	public void Log(Exception exception)
+	{
+		var clientId = GetClientId();
+		var log = new RpcLog(clientId, exception);
+		SendInBackground(log);
+	}
+
+	private string GetClientId() => $"{DeviceInfo.Current.Model}-{DeviceInfo.Current.Version}";
+
+	private void SendInBackground(RpcLog log)
+	{
+		_ = Task.Run(() => EnsureSentLog(log));
+	}
+
+	private async Task EnsureSentLog(RpcLog log)
+	{
+		var result = await Send(log);
+		if (!result.IsSuccessful)
+		{
+			await Task.Delay(TimeSpan.FromSeconds(5));
+			await EnsureSentLog(log);
+		}
+	}
+	private async Task<RpcInvokeResult> Send(RpcLog log)
+	{
+		return await InvokeHubProcedure(nameof(ILoggingHubProcedures.ReceiveLog), log);
+	}
+}
+
+public interface IWitnessLogger
+{
+	void Log(string message);
+	void Log(Exception exception);
+}
