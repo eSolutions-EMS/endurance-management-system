6c8cb3c7296a8fdf5999491c46ead5ebcb53a8d7
Alexander Chobanov <alexander.chobanov@egt-digital.com>
2024-04-16 16:00:00 +0300

    Disconnect the RPC socket OnDeactivated
    Close the connection gracefully. Hopefully this will cause less bugs
    with the connection
    
    https://github.com/Not-Endurance/not-timing-system/issues/179

--- src/Witness/EMS.Witness/App.xaml.cs
+++ src/Witness/EMS.Witness/App.xaml.cs
@@ -10,83 +10,85 @@ namespace EMS.Witness;
 public partial class App : Application, IDisposable
 {
     private readonly IRpcSocket _rpcSocket;
-    private readonly IPersistenceService persistence;
+    private readonly IPersistenceService _persistence;
     private readonly IStartlistClient startlistClient;
     private readonly IStartlistService startlistService;
     private readonly IParticipantsClient participantsClient;
     private readonly IParticipantsService participantsService;
 
     public App(
-        IRpcSocket rpcSocket,
-        IPersistenceService persistence,
-        IStartlistClient startlistClient,
-        IStartlistService startlistService,
-        IParticipantsClient arrivelistClient,
-        IParticipantsService arrivelistService)
-    {
-        this.InitializeComponent();
+		IRpcSocket rpcSocket,
+		IPersistenceService persistence,
+		IStartlistClient startlistClient,
+		IStartlistService startlistService,
+		IParticipantsClient arrivelistClient,
+		IParticipantsService arrivelistService)
+	{
+		this.InitializeComponent();
         this.MainPage = new MainPage();
         _rpcSocket = rpcSocket;
-        this.persistence = persistence;
+        _persistence = persistence;
         this.startlistClient = startlistClient;
         this.startlistService = startlistService;
         this.participantsClient = arrivelistClient;
         this.participantsService = arrivelistService;
     }
 
-    protected override Window CreateWindow(IActivationState? activationState)
-    {
-        var window = base.CreateWindow(activationState);
+	protected override Window CreateWindow(IActivationState? activationState) 
+	{
+		var window = base.CreateWindow(activationState);
 
-        this.AttachEventHandlers();
+		this.AttachEventHandlers();
 
-        window.Deactivated += StoreState;
-        window.Destroying += DetachEventHandlers;
+		window.Deactivated += OnDeactivated;
+		window.Destroying += DetachEventHandlers;
 
-        return window;
-    }
+		return window;
+	}
 
-    public void Dispose()
-    {
-        throw new NotImplementedException();
-    }
+	public void Dispose()
+	{
+		throw new NotImplementedException();
+	}
 
-    private async void StoreState(object? sender, EventArgs args)
-    {
-        await persistence.Store();
-    }
+    private async void OnDeactivated(object? sender, EventArgs args)
+	{
+		await Task.WhenAll(
+			_persistence.Store(),
+			_rpcSocket.Disconnect());
+	}
 
-    private void AttachEventHandlers()
-    {
-        //TODO: Add Initialize method to StartlistService ParticipantService to attach necessary events
-        this.participantsClient.Updated += HandleParticipantsUpdate;
-        this.startlistClient.Updated += HandleStartlistUpdate;
+	private void AttachEventHandlers()
+	{
+		//TODO: Add Initialize method to StartlistService ParticipantService to attach necessary events
+		this.participantsClient.Updated += HandleParticipantsUpdate;
+		this.startlistClient.Updated += HandleStartlistUpdate;
         _rpcSocket.ServerConnectionChanged += HandleSocketConnectionChanged;
     }
 
     private void DetachEventHandlers(object? sender, EventArgs args)
-    {
-        this.participantsClient.Updated -= HandleParticipantsUpdate;
-        this.startlistClient.Updated -= HandleStartlistUpdate;
+	{
+		this.participantsClient.Updated -= HandleParticipantsUpdate;
+		this.startlistClient.Updated -= HandleStartlistUpdate;
         _rpcSocket.ServerConnectionChanged -= HandleSocketConnectionChanged;
     }
 
-    private void HandleParticipantsUpdate(object? sender, (ParticipantEntry Participant, CollectionAction Action) args)
-    {
-        this.participantsService.Update(args.Participant, args.Action);
-    }
+	private void HandleParticipantsUpdate(object? sender, (ParticipantEntry Participant, CollectionAction Action) args)
+	{
+		this.participantsService.Update(args.Participant, args.Action);
+	}
 
-    private void HandleStartlistUpdate(object? sender, (StartlistEntry StartlistEntry, CollectionAction Action) args)
-    {
-        this.startlistService.Update(args.StartlistEntry, args.Action);
-    }
+	private void HandleStartlistUpdate(object? sender, (StartlistEntry StartlistEntry, CollectionAction Action) args)
+	{
+		this.startlistService.Update(args.StartlistEntry, args.Action);
+	}
 
-    private async void HandleSocketConnectionChanged(object? sender, RpcConnectionStatus status)
-    {
-        if (status == RpcConnectionStatus.Connected)
-        {
-            await this.startlistService.Load();
-            await this.participantsService.Load();
+	private async void HandleSocketConnectionChanged(object? sender, RpcConnectionStatus status)
+	{
+		if (status == RpcConnectionStatus.Connected)
+		{
+			await this.startlistService.Load();
+			await this.participantsService.Load();
         }
-    }
+	}
 }
