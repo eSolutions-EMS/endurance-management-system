265360d4c8346d9571d6f4be723b9af2dfdb8485
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-05-13 17:51:16 +0300

    Modify Phase Entity to support separate inputs
    Add Create, Update methods and appropriate ctors
    
    https://github.com/Not-Endurance/not-timing-system/issues/210

--- src-v2/NTS.Domain.Setup/Entities/Phase.cs
+++ src-v2/NTS.Domain.Setup/Entities/Phase.cs
@@ -3,7 +3,14 @@
 namespace NTS.Domain.Setup.Entities;
 public class Phase : DomainEntity
 {
+    public static Phase Create(double distance) => new (distance);
+    public static Phase Update(int id, double distance) => new(id, distance);
+
     [JsonConstructor]
+    public Phase(int id, double distance) : this(distance)
+    {
+        Id = id;
+    }
     public Phase(double distance)
     {
         if (distance <= 0)
