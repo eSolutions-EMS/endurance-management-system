318b0fa79523d0c37562343def27ac139b2c67b6
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-05-17 18:18:33 +0300

    Implement LapBehind and apply necessary changes to Components
    These changes implement the LapBehind structure in practice
    
    https://github.com/Not-Endurance/not-timing-system/issues/210

--- src-v2/Judge/NTS.Judge.Blazor/Pages/Setup/Phases/PhaseFormModel.cs
+++ src-v2/Judge/NTS.Judge.Blazor/Pages/Setup/Phases/PhaseFormModel.cs
@@ -17,13 +17,13 @@ public class PhaseFormModel
     public PhaseFormModel(Phase phase)
     {
         Id = phase.Id;
-        SelectedLap = (int)phase.SelectedLap!;
+        SelectedLap = phase.SelectedLap;
         Recovery = (int)phase.Recovery!;
         Rest = (int)phase.Rest!;
     }
 
     public int Id { get; set; }
-    public int SelectedLap { get; set; }
+    public Lap SelectedLap { get; set; }
     public int Recovery { get; set; }
     public int Rest { get; set; }
 }
---
+++ src-v2/Judge/NTS.Judge/Events/LapBehind.cs
@@ -0,0 +1,38 @@
+﻿using Not.Application.Ports.CRUD;
+using Not.Blazor.Ports.Behinds;
+using Not.Domain;
+using Not.Exceptions;
+using NTS.Domain.Setup.Entities;
+using static MudBlazor.CategoryTypes;
+
+
+namespace NTS.Judge.Events;
+public class LapBehind : INotSetBehind<Lap>
+{
+    private readonly ISetRepository<Lap> _lapRepository;
+    private Lap? _lap;
+
+    public LapBehind(ISetRepository<Lap> loopRepository)
+    {
+        _lapRepository = loopRepository;
+    }
+    public Task<IEnumerable<Lap>> GetAll()
+    {
+        return _lapRepository.ReadAll();
+    }
+    public async Task<Lap> Create(Lap entity)
+    {
+        _lap = await _lapRepository.Create(entity);
+        return _lap;
+    }
+
+    public async Task<Lap> Update(Lap entity)
+    {
+        return await _lapRepository.Update(entity);
+    }
+
+    public async Task<Lap> Delete(Lap entity)
+    {
+        return await _lapRepository.Delete(entity);
+    }
+}
