430f91b22a2cc7c13fc60ecdb28d599f07211963
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-06-20 01:22:19 +0300

    Create HorseForm Blazor component
    Includes HorseFormModel.cs, HorseFormUpdate.razor, an addition to
    Endpoints.cs and a modification of the Horse Setup Domain Entity.
    
    https://github.com/Not-Endurance/not-timing-system/issues/223

--- src-v2/Judge/NTS.Judge.Blazor/Pages/Endpoints.cs
+++ src-v2/Judge/NTS.Judge.Blazor/Pages/Endpoints.cs
@@ -8,5 +8,6 @@ public static class Endpoints
     public const string COMPETITION_UPDATE = "/competition-update";
     public const string CONTESTANT_UPDATE = "/contestant-update";
     public const string PHASE_UPDATE = "/phase-update";
+    public const string HORSE_UPDATE = "/horse-update";
     public const string DASHBOARD_PAGE = "/dashboard";
 }
