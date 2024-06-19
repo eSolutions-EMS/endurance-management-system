b5fd1a3ffede07b17db4aea72d670d7275a71a06
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-06-20 01:25:03 +0300

    Implement HorseBehind and HorseRepository for persistence
    Also implementing the ISetState<Horse> interface in SetupState
    
    https://github.com/Not-Endurance/not-timing-system/issues/223

---
+++ src-v2/NTS.Persistance/Adapters/HorseRepository.cs
@@ -0,0 +1,15 @@
+﻿using NTS.Domain.Setup.Entities;
+using NTS.Persistence.Setup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTS.Persistence.Adapters;
+public class HorseRepository : SetRepository<Horse, SetupState>
+{
+    public HorseRepository(IStore<SetupState> store) : base(store)
+    {
+    }
+}
