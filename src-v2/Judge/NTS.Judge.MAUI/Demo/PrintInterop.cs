e8b8679c5ba529e4873380828e8dccff6e0c1277
achobanov <alexchobanov@gmail.com>
2024-07-08 23:51:04 +0300

    Restore PrintInterop implementation

---
+++ src-v2/Judge/NTS.Judge.MAUI/Demo/PrintInterop.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop;
+using Not.Blazor.Print;
+
+namespace NTS.Judge.MAUI.Demo;
+
+public class PrintInterop : IPrintInterop
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public PrintInterop(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public async Task Print()
+    {
+        var module = await _jsRuntime.InvokeAsync<IJSObjectReference>("import", "./scripts/print-custom.js");
+        await module.InvokeVoidAsync("printCustom");
+    }
+}
