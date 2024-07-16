dd43abdd034ebb95c5b9d19e98dd85e58202b3ed
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-07-16 17:50:23 +0300

    Implement CombinationBehind and persistence
    https://github.com/Not-Endurance/not-timing-system/issues/207

---
+++ src-v2/NTS.Persistance/Adapters/CombinationRepository.cs
@@ -0,0 +1,15 @@
+﻿using NTS.Domain.Setup.Entities;
+using NTS.Persistence.Setup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTS.Persistence.Adapters;
+public class CombinationRepository : SetRepository<Combination, SetupState>
+{
+    public CombinationRepository(IStore<SetupState> store) : base(store)
+    {
+    }
+}
--- src-v2/NTS.Persistance/States/SetupState.cs
+++ src-v2/NTS.Persistance/States/SetupState.cs
@@ -2,12 +2,13 @@
 
 namespace NTS.Persistence.Setup;
 
-public class SetupState : NotState, ITreeState<Event>, ISetState<Loop>, ISetState<Horse>, ISetState<Athlete>
+public class SetupState : NotState, ITreeState<Event>, ISetState<Loop>, ISetState<Horse>, ISetState<Athlete>, ISetState<Combination>
 {
     public Event? Event { get; set; }
     public List<Loop> Loops { get; } = new List<Loop>();
     public List<Horse> Horses { get; } = new List<Horse>();
     public List<Athlete> Athletes { get; } = new List<Athlete>();
+    public List<Combination> Combinations { get; } = new List<Combination>();
 
     Event? ITreeState<Event>.Root
     {
@@ -18,4 +19,5 @@ public class SetupState : NotState, ITreeState<Event>, ISetState<Loop>, ISetStat
     List<Loop> ISetState<Loop>.EntitySet => Loops;
     List<Horse> ISetState<Horse>.EntitySet => Horses;
     List<Athlete> ISetState<Athlete>.EntitySet => Athletes;
+    List<Combination> ISetState<Combination>.EntitySet => Combinations;
 }
