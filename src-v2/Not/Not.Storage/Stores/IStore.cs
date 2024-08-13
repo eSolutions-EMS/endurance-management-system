ff0612924804180f08764e4d30228cb2a85407f3
achobanov <alexchobanov@gmail.com>
2024-08-14 01:44:14 +0300

    Fix concurrency issues with store file access
    While not ideal to lock before readonly access, it's the easiest current
    solution to solve concurrency issues when multiple thread simultaneously
    try to read/write to state files
    
    https://github.com/Not-Endurance/not-timing-system/issues/254

--- src-v2/Not/Not.Storage/Stores/IStore.cs
+++ src-v2/Not/Not.Storage/Stores/IStore.cs
@@ -6,7 +6,7 @@ namespace Not.Storage.Stores;
 public interface IStore<T>
     where T : class, IState, new()
 {
-    public Task<T> Readonly();
+    public Task<T> Readonly([CallerFilePath] string callerPath = default!, [CallerMemberName] string callerMember = default!);
     public Task<T> Transact([CallerFilePath] string callerPath = default!, [CallerMemberName] string callerMember = default!);
     public Task Commit(T state);
 }
