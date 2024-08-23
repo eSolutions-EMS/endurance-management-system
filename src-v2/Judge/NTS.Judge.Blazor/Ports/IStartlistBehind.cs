67765128546aab563eb2b239d4e906410f4e300d
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-08-23 11:42:32 +0300

    create StartlistBehind
    https://github.com/Not-Endurance/not-timing-system/issues/252

---
+++ src-v2/Judge/NTS.Judge.Blazor/Ports/IStartlistBehind.cs
@@ -0,0 +1,10 @@
+﻿using Not.Blazor.Ports.Behinds;
+using NTS.Domain.Core.Objects;
+
+namespace NTS.Judge.Blazor.Ports;
+
+public interface IStartlistBehind : IObservableBehind
+{
+    IEnumerable<PhaseStart> Startlist { get; }
+    IEnumerable<IGrouping<double, PhaseStart>> StartlistByDistance { get; }
+}
---
+++ src-v2/Judge/NTS.Judge/Adapters/Behinds/StartlistBehind.cs
@@ -0,0 +1,35 @@
+﻿using Not.Application.Ports.CRUD;
+using NTS.Domain.Core.Aggregates.Participations;
+using NTS.Domain.Core.Objects;
+using NTS.Judge.Blazor.Ports;
+
+namespace NTS.Judge.Adapters.Behinds;
+public class StartlistBehind : IStartlistBehind
+{
+    private readonly IRepository<Participation> _participations;
+
+    public StartlistBehind(IRepository<Participation> participations)
+    {
+        _participations = participations;
+    }
+
+    public IEnumerable<PhaseStart> Startlist { get; private set; } = new List<PhaseStart>();
+    public IEnumerable<IGrouping<double, PhaseStart>> StartlistByDistance => Startlist.GroupBy(x => x.Distance);
+
+
+    public async Task Initialize()
+    {
+        var participations = await _participations.ReadAll();
+        var startlist = new List<PhaseStart>();
+        foreach (var participation in participations)
+        {
+            var phaseStart = new PhaseStart(participation);
+            Startlist.ToList().Add(phaseStart);
+        }
+    }
+
+    public void Subscribe(Func<Task> action)
+    {
+        throw new NotImplementedException();
+    }
+}
