422c82e290277644daeb7974a6506add3383795a
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-08-27 17:15:12 +0300

    implement NotTimer and use it for StartIn column at StartlistPage
    https://github.com/Not-Endurance/not-timing-system/issues/252

--- src-v2/NTS.Domain.Core/Objects/Start.cs
+++ src-v2/NTS.Domain.Core/Objects/Start.cs
@@ -10,15 +10,15 @@ public class Start
 {
     public Start(Participation participation)
     {
-            var phaseStart = new PhaseStart(participation);
-            Number = phaseStart.Number;
-            Athlete = phaseStart.Athlete;
-            LoopNumber = phaseStart.LoopNumber;
-            Distance = phaseStart.Distance;
-            StartAt = phaseStart.StartAt;
-            GuardHelper.ThrowIfDefault(participation?.Phases?.Current);
-            CurrentPhase = participation?.Phases?.Current;
-            TotalDistance = participation?.Phases?.Distance;
+        var phaseStart = new PhaseStart(participation);
+        Number = phaseStart.Number;
+        Athlete = phaseStart.Athlete;
+        LoopNumber = phaseStart.LoopNumber;
+        Distance = phaseStart.Distance;
+        StartAt = phaseStart.StartAt;
+        GuardHelper.ThrowIfDefault(participation?.Phases?.Current);
+        CurrentPhase = participation?.Phases?.Current;
+        TotalDistance = participation?.Phases?.Distance;
     }
 
     public int Number { get; private set; }
@@ -28,11 +28,25 @@ public class Start
     public Phase CurrentPhase { get; private set; }
     public double? TotalDistance { get; private set; }
     public Timestamp StartAt { get; private set; }
-    public TimeInterval StartIn => StartAt - Timestamp.Now();
+    public string StartIn()
+    {
+        if (StartAt > Timestamp.Now())
+        {
+            return (StartAt - Timestamp.Now()).ToString();
+        }
+        else
+        {
+            return "-" + (StartAt - Timestamp.Now()).ToString();
+        }
+        //test cases - remove before merge      
+        //return "00:05:02";
+        //return "00:00:03";
+        //return "-00:14:57";
+    }
 
     public override string ToString()
     {
-        var result = $"{Number}, {Athlete}, #{LoopNumber}: {Distance}km , {StartAt}, {StartIn}";
+        var result = $"{Number}, {Athlete}, #{LoopNumber}: {Distance}km , {StartAt}";
         return result;
     }
 }
