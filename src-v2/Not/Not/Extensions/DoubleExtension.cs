52883a7291d4a1b737cb9b05777b4740cbe77141
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-08-27 18:31:36 +0300

    implement FloorWholeNumberToTens method at DoubleExtension
    use the method to display the Competition distance roundedto tens
    
    https://github.com/Not-Endurance/not-timing-system/issues/252

---
+++ src-v2/Not/Not/Extensions/DoubleExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace Not.Extensions;
+public static class DoubleExtension
+{
+    public static double FloorWholeNumberToTens(this double number)
+    {
+        return (double)Math.Floor((decimal)number / 10) * 10;
+    }
+
+    public static double FloorWholeNumberToTens(this double? number)
+    {
+        if(number == null) throw new ArgumentNullException(nameof(number));
+        return (double)Math.Floor((decimal)number / 10) * 10;
+    }
+}
