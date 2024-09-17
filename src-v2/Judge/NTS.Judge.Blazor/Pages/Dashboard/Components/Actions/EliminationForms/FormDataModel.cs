f3d3789a4ff3d1fc0138afb4b82b00dbb2e02a37
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-09-17 12:42:22 +0300

    add separate Forms for Eliminations

---
+++ src-v2/Judge/NTS.Judge.Blazor/Pages/Dashboard/Components/Actions/EliminationForms/FormDataModel.cs
@@ -0,0 +1,26 @@
+﻿using NTS.Domain.Core.Aggregates.Participations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTS.Judge.Blazor.Pages.Dashboard.Components.Actions.EliminationForms;
+public class FormData
+{
+    public string EliminationCode { get; set; }
+    public int TandemNumber { get; set; }
+    public string? Reason { get; set; }
+    public List<FTQCodes> Codes { get; set; } = new List<FTQCodes>();
+
+    public FormData(string eliminationCode, int number, string? reason, IEnumerable<string> selected_values)
+    {
+        EliminationCode = eliminationCode;
+        TandemNumber = number;
+        Reason = reason;
+        foreach (var value in selected_values)
+        {
+            Codes.Add((FTQCodes)Enum.Parse(typeof(FTQCodes), value));
+        }
+    }
+}
