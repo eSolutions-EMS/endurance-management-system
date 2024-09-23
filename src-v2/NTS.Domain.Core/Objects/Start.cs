1fb8d25516aaf4e3643f738690fa8b5949566411
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-09-23 10:26:13 +0300

    create StartHistory class`

--- src-v2/NTS.Domain.Core/Objects/Start.cs
+++ src-v2/NTS.Domain.Core/Objects/Start.cs
@@ -1,32 +1,74 @@
 ﻿using NTS.Domain.Core.Aggregates.Participations;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NTS.Domain;
 
 namespace NTS.Domain.Core.Objects;
+
+public class StartHistory
+{
+    public StartHistory(IEnumerable<Participation> participations)
+    {
+        foreach (var participation in participations)
+        {
+            foreach (var phase in participation.Phases)
+            {
+                var loopNumber = participation.Phases.NumberOf(phase ?? participation.Phases.Last());
+                var previousStart = new Start(participation.Tandem.Name, participation.Tandem.Number, loopNumber, phase.Length, participation.Phases.Distance, phase, phase.OutTime);
+                PreviousStarts.Add(previousStart);
+            }
+        }
+    }
+    public List<Start> PreviousStarts { get; private set; } = new List<Start>();
+    public IEnumerable<IGrouping<double, Start>> StartlistByDistance => PreviousStarts.GroupBy(p => p.Distance);
+}
+
+public class StartList
+{
+    public StartList(IEnumerable<Participation> participations)
+    {
+        foreach(var participation in participations)
+        {
+            var phase = participation.Phases.Current;
+            var loopNumber = participation.Phases.NumberOf(phase ?? participation.Phases.Last());
+            var upcomingStart = new Start(participation.Tandem.Name, participation.Tandem.Number, loopNumber, phase.Length, participation.Phases.Distance, phase, phase.OutTime);
+            UpcomingStarts.Add(upcomingStart);
+        }
+    }
+    public Person Athlete { get; private set; }
+    public List<Start> UpcomingStarts = new List<Start>();
+    IEnumerable<IGrouping<double?, Start>> StartlistByDistance { get; }
+}
 public class Start
 {
     public Start(Participation participation)
     {
         var phaseStart = new PhaseStart(participation);
-        Number = phaseStart.Number;
-        Athlete = phaseStart.Athlete;
+        Athlete = participation.Tandem.Name;
+        Number = participation.Tandem.Number;
         LoopNumber = phaseStart.LoopNumber;
-        Distance = phaseStart.Distance;
+        Distance = participation.Phases.Current.Length;
         StartAt = phaseStart.StartAt;
         GuardHelper.ThrowIfDefault(participation?.Phases?.Current);
         CurrentPhase = participation?.Phases?.Current;
         TotalDistance = participation?.Phases?.Distance;
     }
 
-    public int Number { get; private set; }
+    public Start(Person athlete, int number, int loopNumber, double distance, double? totalDistance, Phase phase, Timestamp startAt)
+    {
+        Athlete = athlete;
+        Number = number;
+        LoopNumber = loopNumber;
+        Distance = distance;
+        TotalDistance = totalDistance;
+        CurrentPhase = phase;
+        StartAt = startAt;
+    }
+
     public Person Athlete { get; private set; }
+    public int Number { get; private set; }
     public int LoopNumber { get; private set; }
     public double Distance { get; private set; }
-    public Phase CurrentPhase { get; private set; }
     public double? TotalDistance { get; private set; }
+    public Phase CurrentPhase { get; private set; }
     public Timestamp StartAt { get; private set; }
     public string StartIn()
     {
@@ -38,7 +80,7 @@ public class Start
         {
             return "-" + (StartAt - Timestamp.Now()).ToString();
         }
-        //test cases - remove before merge      
+        //test cases -remove before merge
         //return "00:05:02";
         //return "00:00:03";
         //return "-00:14:57";
@@ -46,7 +88,7 @@ public class Start
 
     public override string ToString()
     {
-        var result = $"{Number}, {Athlete}, #{LoopNumber}: {Distance}km , {StartAt}";
+        var result = $"{Number}, {Athlete}, #{LoopNumber}: {Distance}km, {StartAt}";
         return result;
     }
 }
