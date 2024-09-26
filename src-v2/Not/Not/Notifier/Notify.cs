4bee22b9dfcb4d692d2c179ac5014bcaad61161d
achobanov <alexchobanov@gmail.com>
2024-09-26 14:07:55 +0300

    Rename Notify methods
    https://github.com/Not-Endurance/not-timing-system/issues/287

--- src-v2/Not/Not/Notifier/Notify.cs
+++ src-v2/Not/Not/Notifier/Notify.cs
@@ -1,26 +1,34 @@
 ﻿using Not.Events;
+using Not.Exceptions;
+using System;
 
 namespace Not.Notifier;
 
 public static class Notify
 {
-    public static void Failed(string message)
-    {
-        EventHelper.Emit(new Failed(message));
-    }
-
-    public static void Informed(string message)
+    public static void Inform(string message)
     {
         EventHelper.Emit(new Informed(message));
     }
 
-    public static void Succeeded(string message)
+    public static void Success(string message)
     {
         EventHelper.Emit(new Succeeded(message));
     }
 
-    public static void Warned(string message)
+    public static void Warn(string message)
     {
         EventHelper.Emit(new Warned(message));
     }
+
+    public static void Warn(DomainExceptionBase validation)
+    {
+        Warn(validation.Message);
+    }
+
+    public static void Error(Exception exception)
+    {
+        EventHelper.Emit(new Failed(exception.Message + Environment.NewLine + exception.StackTrace));
+    }
+
 }
