461ab810592587d344d4d2435334ca70a1c09c10
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-09-28 20:15:17 +0300

    create mock ITagWriter interface and TagWriterBehind
    https://github.com/Not-Endurance/not-timing-system/issues/224

---
+++ src-v2/Judge/NTS.Judge.Blazor/Ports/ITagWriter.cs
@@ -0,0 +1,8 @@
+﻿using Not.Injection;
+using NTS.Domain.Setup.Entities;
+
+namespace NTS.Judge.Blazor.Ports;
+public interface ITagWriter : ITransientService
+{
+    Task<IdTag> WriteTag(int number);
+}
