7c92bc437ef9b06ab683b2e5c68dac70d0b1368e
achobanov <alexchobanov@gmail.com>
2024-09-28 21:37:29 +0300

    Remove localization from DomainException
    Otherwise we will eventually end up with double localization
    
    https://github.com/Not-Endurance/not-timing-system/issues/287

--- src-v2/Not/Not.Domain/DomainException.cs
+++ src-v2/Not/Not.Domain/DomainException.cs
@@ -1,38 +1,17 @@
 ﻿using Not.Exceptions;
-using Not.Localization;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Not.Domain;
 
 //TODO: Create AggregateDomainException and modify domains to batch their validation exceptions before throwing
 public class DomainException : DomainExceptionBase
 {
-    public DomainException(string property, string message) : base(message.Localize())
+    public DomainException(string property, string message) : base(message)
     {
         Property = property;
     }
-    public DomainException(
-        string property,
-        [StringSyntax(StringSyntaxAttribute.CompositeFormat)] string message,
-        params string[] args) : this(message, args)
-    {
-        Property = property;
-    }
-    public DomainException(
-        [StringSyntax(StringSyntaxAttribute.CompositeFormat)] string message,
-        params string[] args) : base(LocalizeAndFormat(message, args))
-    {
-    }
-    public DomainException(string message) : base(message.Localize())
+    public DomainException(string message) : base(message)
     {
     }
 
     public string? Property { get; }
-
-    static string LocalizeAndFormat(string template, string[] args)
-    {
-        template = template.Localize();
-        args = args.Localize().ToArray();
-        return string.Format(template, args);
-    }
 }
