8f05a06ffc774a2818a8bf5bc86fd2c2b7051c4e
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-09-28 18:29:54 +0300

    resolve conflicts and modify IdTag

--- src-v2/NTS.Domain.Setup/Entities/IdTag.cs
+++ src-v2/NTS.Domain.Setup/Entities/IdTag.cs
@@ -2,19 +2,25 @@
 
 public class IdTag : DomainEntity, ISummarizable
 {
-    public IdTag(string tagId, string position, int number)
+    //remove when integrating controllers
+    //public IdTag(string tagData)
+    //{
+    //    var number = tagData.Substring(0, 3);
+    //    var id = tagData.Substring(9);
+    //    Id = int.Parse(id);
+    //    Number = int.Parse(number);
+    //}
+    public IdTag(string tagId, int number)
     {
 		TagId = tagId;
-		Position = position;
         Number = number;
     }
 
     public string TagId { get; private set; }
-	public string Position { get; private set; }
     public int Number { get; }
 
     public override string ToString()
 	{
-		return Combine($"{"id".Localize()}: {this.TagId}", $"{"position".Localize()}: {this.Position}");
-	}
+        return Combine($"{"id".Localize()}: {TagId}");
+    }
 }
