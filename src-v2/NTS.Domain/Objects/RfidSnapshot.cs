12d804503401efd1fc5ca67cc007b3a95321d9fd
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-10-01 15:43:14 +0300

    create RfidSnapshot class at NTS.Domain.Objects
    https://github.com/Not-Endurance/not-timing-system/issues/281

---
+++ src-v2/NTS.Domain/Objects/RfidSnapshot.cs
@@ -0,0 +1,19 @@
+﻿using NTS.Domain.Enums;
+
+namespace NTS.Domain.Objects;
+
+public record RfidSnapshot : DomainObject, ISnapshot
+{
+    public RfidSnapshot(int number, SnapshotType type, Timestamp timestamp)
+    {
+        Number = number;
+        Type = type;
+        Method = SnapshotMethod.RFID;
+        Timestamp = timestamp;
+    }
+    public int Number { get; }
+    public SnapshotType Type { get; }
+    public SnapshotMethod Method { get; }
+    public Timestamp Timestamp { get; }
+}
+
