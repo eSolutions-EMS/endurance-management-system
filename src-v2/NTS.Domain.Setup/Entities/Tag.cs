66f9b29a274ee52f936a8ce036e3c088cef76c53
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-10-01 15:41:57 +0300

    add PrepareToWrite method to Setup Tag Entity

--- src-v2/NTS.Domain.Setup/Entities/Tag.cs
+++ src-v2/NTS.Domain.Setup/Entities/Tag.cs
@@ -19,6 +19,14 @@ public class Tag : DomainEntity, ISummarizable
     public string TagId { get; private set; }
     public int Number { get; }
 
+    public string PrepareToWrite()
+    {
+        const char EMPTY_CHAR = '0';
+        var number = Number.ToString().PadLeft(3, EMPTY_CHAR);
+        var position = "abcdef".PadLeft(6, EMPTY_CHAR); // present for legacy compatibility
+        return number + position + TagId;
+    }
+
     public override string ToString()
 	{
         return Combine($"{"Tag Id".Localize()}: {TagId}");
