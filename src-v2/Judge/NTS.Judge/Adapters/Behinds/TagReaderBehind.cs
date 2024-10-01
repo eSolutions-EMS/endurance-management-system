05daa170ec0a771fb1028efbc48199b51c10aa55
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-10-01 16:57:59 +0300

    split TagBehind implementation into ReaderBehind and WriterBehind
    also add ITagRead interface
    
    https://github.com/Not-Endurance/not-timing-system/issues/281

---
+++ src-v2/Judge/NTS.Judge/Adapters/Behinds/TagReaderBehind.cs
@@ -0,0 +1,31 @@
+﻿using NTS.Domain;
+using NTS.Domain.Core.Configuration;
+using NTS.Domain.Objects;
+using NTS.Domain.Setup.Entities;
+using NTS.Judge.Blazor.Ports;
+using NTS.Judge.Hardware;
+
+namespace NTS.Judge.Adapters.Behinds;
+
+public class TagReaderBehind : ITagRead
+{
+    private VupVF747pController VF747PController;
+
+    public TagReaderBehind()
+    {
+        VF747PController = new VupVF747pController("192.168.68.128", TimeSpan.FromMilliseconds(10));
+        VF747PController.Connect();
+    }
+
+    public async Task<IEnumerable<RfidSnapshot>> ReadTags()
+    {
+        var tags = new List<RfidSnapshot>();
+        var data_array = await Task.Run(VF747PController.StartReading);
+        foreach(var tagData in data_array)
+        {
+            var number = int.Parse(tagData.Substring(0, 3));
+            tags.Add(new RfidSnapshot(number, StaticOptions.RfidSnapshotType(), new Timestamp(DateTime.Now)));
+        }
+        return tags;
+    }
+}
---
+++ src-v2/Judge/NTS.Judge/Adapters/Behinds/TagWriterBehind.cs
@@ -0,0 +1,27 @@
+﻿using NTS.Domain;
+using NTS.Domain.Core.Configuration;
+using NTS.Domain.Objects;
+using NTS.Domain.Setup.Entities;
+using NTS.Judge.Blazor.Ports;
+using NTS.Judge.Hardware;
+namespace NTS.Judge.Adapters.Behinds;
+
+public class TagWriterBehind : ITagWrite
+{
+    private VupVD67Controller VD67Controller;
+
+    public TagWriterBehind()
+    {
+        VD67Controller = new VupVD67Controller(TimeSpan.FromMilliseconds(100));
+        VD67Controller.Connect();
+    }
+
+    public async Task<Tag> Write(int number)
+    {
+        var data = await Task.Run(VD67Controller.Read);
+        var id = data.Substring(9);
+        var tag = new Tag(id, number);
+        VD67Controller.Write(tag.PrepareToWrite());
+        return tag;
+    }
+}
