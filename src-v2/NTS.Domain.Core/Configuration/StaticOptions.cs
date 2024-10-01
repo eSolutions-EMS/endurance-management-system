b3d7cb2185efa1e36f01913a0ee3e432d184822d
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-10-01 15:07:15 +0300

    add StaticOptions for DetectionConfiguration and use them at CombinationForm
    available DetectionModes are Manual(default), Rfid and ComputerVision
    
    https://github.com/Not-Endurance/not-timing-system/issues/224

--- src-v2/NTS.Domain.Core/Configuration/StaticOptions.cs
+++ src-v2/NTS.Domain.Core/Configuration/StaticOptions.cs
@@ -10,6 +10,7 @@ public class StaticOptions : IStartupInitializer, ISingletonService
     
     static Model? _options;
     public static IRegionalConfiguration? RegionalConfiguration { get; set; }
+    public static DetectionConfiguration? DetectionConfiguration { get; set; }
 
     public static bool ShouldOnlyUseAverageLoopSpeed(CompetitionRuleset ruleset)
     {
@@ -29,6 +30,11 @@ public class StaticOptions : IStartupInitializer, ISingletonService
         return true;
     }
 
+    public static bool RfidDetection()
+    {
+        return DetectionConfiguration.IsRfid();
+    }
+
     static bool ShouldUseRegionalConfiguration(CompetitionRuleset ruleset)
     {
         return ruleset == CompetitionRuleset.Regional && RegionalConfiguration != null;
@@ -43,11 +49,13 @@ public class StaticOptions : IStartupInitializer, ISingletonService
     {
         _options = _provider.Get();
         RegionalConfiguration = RegionalConfigurationProvider.Get(_options.SelectedCountry);
+        DetectionConfiguration = new DetectionConfiguration(_options.DetectionMode);
     }
 
     public class Model
     {
         public Country[] Countries { get; set; } = [];  
         public Country? SelectedCountry { get; set; }
+        public DetectionMode DetectionMode { get; set; }
     }
 }
