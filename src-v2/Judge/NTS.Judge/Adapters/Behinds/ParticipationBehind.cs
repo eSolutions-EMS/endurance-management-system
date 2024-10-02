d39cd80c5cf0f9f2d5c8fb40bdd75ef2786b9649
achobanov <alexchobanov@gmail.com>
2024-10-02 16:46:01 +0300

    Perform particpation sorting on initialization rather then every rander

--- src-v2/Judge/NTS.Judge/Adapters/Behinds/ParticipationBehind.cs
+++ src-v2/Judge/NTS.Judge/Adapters/Behinds/ParticipationBehind.cs
@@ -23,7 +23,6 @@ public class ParticipationBehind : ObservableBehind, IParticipationBehind
     }
 
     public IEnumerable<Participation> Participations { get; private set; } = new List<Participation>();
-    public IEnumerable<IGrouping<double, Participation>> ParticipationsByDistance => Participations.GroupBy(x => x.Phases.Distance);
     public Participation? SelectedParticipation
     {
         get => _selectedParticipation; 
