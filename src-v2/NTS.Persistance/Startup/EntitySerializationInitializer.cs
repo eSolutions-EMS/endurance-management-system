7e90d2b288dbda97418993c825599a8ed13fde8a
achobanov <alexchobanov@gmail.com>
2024-10-03 22:28:46 +0300

    BOG E BYLGARIN!
    Create EntityReferenceEqualityGuardConverter that ensures Doamin
    equality results in reference equality during serializaiton and
    deserialization
    
    Holy Ass Jesus. I've been trying to workaround this issue for years
    now..
    
    https://github.com/Not-Endurance/not-timing-system/issues/301

---
+++ src-v2/NTS.Persistance/Startup/EntitySerializationInitializer.cs
@@ -0,0 +1,18 @@
+﻿using Not.Injection;
+using Not.Serialization;
+using Not.Startup;
+using NTS.Domain.Setup.Entities;
+using NTS.Persistence.Converters;
+
+namespace NTS.Persistence.Startup;
+
+public class EntitySerializationInitializer : IStartupInitializer, ITransientService
+{
+    public void RunAtStartup()
+    {
+        SerializationExtensions.AddConverter(new EntityReferenceEqualityGuardConverter<Horse>());
+        SerializationExtensions.AddConverter(new EntityReferenceEqualityGuardConverter<Athlete>());
+        SerializationExtensions.AddConverter(new EntityReferenceEqualityGuardConverter<Combination>());
+        SerializationExtensions.AddConverter(new EntityReferenceEqualityGuardConverter<Loop>());
+    }
+}
