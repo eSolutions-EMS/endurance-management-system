f56120ced8d1ce8c9be40ee26d8a3b3d20ef3b9f
achobanov <alexchobanov@gmail.com>
2024-10-08 22:24:18 +0300

    Remove obsolete NotSetList, NotDialogs, NotFormNavigator
    These are effectively replaced by CrudList, DialogTM, FormManager
    
    https://github.com/Not-Endurance/not-timing-system/issues/267

--- src-v2/Not/Not.Blazor/TM/Dialogs/NotDialogs.cs
+++ src-v2/Not/Not.Blazor/TM/Dialogs/NotDialogs.cs
@@ -1,30 +1,9 @@
 ﻿using Not.Services;
 using MudBlazor;
-using Not.Blazor.Dialogs;
 using Not.Blazor.TM.Forms.Components;
 using Not.Reflection;
 
 namespace Not.Blazor.TM.Dialogs;
-
-public class NotDialogs<T, TForm> : IDialogs<T, TForm>
-    where TForm : FormTM<T>
-{
-    private readonly IDialogService _mudDialogService;
-    private readonly ILocalizer _localizer;
-
-    public NotDialogs(IDialogService mudDialogService, ILocalizer localizer)
-    {
-        _mudDialogService = mudDialogService;
-        _localizer = localizer;
-    }
-
-    public async Task RenderCreate()
-    {
-        var title = _localizer.Get("Create", " ", ReflectionHelper.GetName<T>());
-        var dialog = await _mudDialogService.ShowAsync<NotFormCreate<T, TForm>>(title);
-        await dialog.Result;
-    }
-}
 
 public class DialogTM<T, TForm>
     where T : new()
--- src-v2/Not/Not.Blazor/TM/Forms/NotFormNavigator.cs
+++ src-v2/Not/Not.Blazor/TM/Forms/NotFormNavigator.cs
@@ -1,36 +1,8 @@
-﻿using Not.Domain;
-using Not.Blazor.Dialogs;
-using Not.Blazor.Forms;
-using Not.Blazor.Navigation;
+﻿using Not.Blazor.Navigation;
 using Not.Blazor.TM.Forms.Components;
 using Not.Blazor.TM.Dialogs;
 
 namespace Not.Blazor.TM.Forms;
-
-public class NotFormNavigator<T, TFields> : IFormNavigator<T, TFields>
-    where T : DomainEntity
-    where TFields : FormTM<T>
-{
-    private readonly IDialogs<T, TFields> _notDialogs;
-    private readonly ICrumbsNavigator _navigator;
-
-    public NotFormNavigator(IDialogs<T, TFields> notDialogs, ICrumbsNavigator navigator)
-    {
-        _notDialogs = notDialogs;
-        _navigator = navigator;
-    }
-
-    public async Task Create()
-    {
-        await _notDialogs.RenderCreate();
-    }
-
-    public Task Update(string endpoint, T entity)
-    {
-        _navigator.NavigateTo(endpoint, entity);
-        return Task.CompletedTask;
-    }
-}
 
 public class FormManager<T, TForm>
     where T : new()
