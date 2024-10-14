cefaa1ca422bde7fbb8986f29f244e6912dd6193
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-10-15 00:31:59 +0300

    @NTS.TagRfidControllers move Logger and Tag classes in Entities namespace
    https://github.com/Not-Endurance/not-timing-system/issues/281

---
+++ src-v2/NTS.TagRfidControllers/Entities/Logger.cs
@@ -0,0 +1,10 @@
+﻿using NTS.TagRfidControllers.HardwareControllers;
+
+namespace NTS.TagRfidControllers.Entities;
+public class Logger : ILogger
+{
+    public void Log(string name, string error)
+    {
+        Console.WriteLine(name, error);
+    }
+}
