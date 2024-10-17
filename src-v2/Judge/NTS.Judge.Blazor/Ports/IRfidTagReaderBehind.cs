7c3832a956b256f9b6eff6e011a211655e4a61b0
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-10-17 16:04:12 +0300

    implement RfidTagReaderBehind

--- src-v2/Judge/NTS.Judge.Blazor/Ports/IRfidTagReaderBehind.cs
+++ src-v2/Judge/NTS.Judge.Blazor/Ports/IRfidTagReaderBehind.cs
@@ -1,9 +1,4 @@
 ﻿using Not.Injection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NTS.Judge.Blazor.Ports;
 public interface IRfidTagReaderBehind : ISingletonService
