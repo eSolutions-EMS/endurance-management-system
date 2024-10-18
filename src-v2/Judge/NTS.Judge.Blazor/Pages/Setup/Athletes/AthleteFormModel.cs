7e4a1d48b51c9d49d1390a844943fb1b16b36007
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-10-18 17:00:31 +0300

    separate Competition Type and Competition Ruleset properties
    https://github.com/Not-Endurance/not-timing-system/issues/282

--- src-v2/Judge/NTS.Judge.Blazor/Pages/Setup/Athletes/AthleteFormModel.cs
+++ src-v2/Judge/NTS.Judge.Blazor/Pages/Setup/Athletes/AthleteFormModel.cs
@@ -9,7 +9,6 @@ public class AthleteFormModel
         // mock data for testing
         Name = "Gucci Petrov";
         Club = "Конярче ООД";
-        Category = AthleteCategory.Senior; //use Senier as the default value of the FormModel prop
         Country = new Country("BG", "zz", "Bulgaria");
     }
     public AthleteFormModel(Athlete athlete)
@@ -27,6 +26,6 @@ public class AthleteFormModel
     public string? FeiId { get; set; }
     public Country Country { get; set; }
     public string Club { get; set; }
-    public AthleteCategory Category { get; set; }
+    public AthleteCategory Category { get; set; } = AthleteCategory.Senior;
 
 }
---
+++ src-v2/NTS.Domain/Enums/CompetitionType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NTS.Domain.Enums;
+
+public enum CompetitionType
+{
+    [Display(Name = "Qualification")]
+    Qualification = 1,
+    [Display(Name = "Star Level")]
+    Star = 2
+}
