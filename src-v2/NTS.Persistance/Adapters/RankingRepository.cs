f3a5b7ae8850a45503986104150f6b5d42c7e80e
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-10-18 23:09:03 +0300

    add Ranking repository
    https://github.com/Not-Endurance/not-timing-system/issues/282

---
+++ src-v2/NTS.Persistance/Adapters/RankingRepository.cs
@@ -0,0 +1,8 @@
+﻿using NTS.Domain.Core.Entities;
+using NTS.Persistence.States;
+
+namespace NTS.Persistence.Adapters;
+
+public class RankingRepository(IStore<CoreState> store) : SetRepository<Ranking, CoreState>(store)
+{
+}
