e28b418d840e05e2a0b4ac6f1b09e3a88929b2da
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-10-21 13:24:04 +0300

    rename ClassificationRepository to Ranking
    https://github.com/Not-Endurance/not-timing-system/issues/282

--- src-v2/NTS.Persistance/Adapters/ClassificationRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using NTS.Domain.Core.Entities;
-using NTS.Persistence.States;
-
-namespace NTS.Persistence.Adapters;
-
-public class ClassificationRepository(IStore<CoreState> store) : SetRepository<Ranking, CoreState>(store)
-{
-}
