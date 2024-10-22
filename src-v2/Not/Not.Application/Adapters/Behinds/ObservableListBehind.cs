64f2c43c83339de523d57e638eb881663a6c829c
achobanov <alexchobanov@gmail.com>
2024-10-22 17:08:28 +0300

    Fix ObservableListBehind EmitChange subscription

--- src-v2/Not/Not.Application/Adapters/Behinds/ObservableListBehind.cs
+++ src-v2/Not/Not.Application/Adapters/Behinds/ObservableListBehind.cs
@@ -2,17 +2,27 @@
 
 namespace Not.Application.Adapters.Behinds;
 
-public abstract class ObservableListBehind<T> : ObservableBehind
+public abstract class ObservableListBehind<T> : ObservableBehind, IDisposable
     where T : IIdentifiable
 {
+    readonly Guid _loadedSubscriptionId;
+
     protected ObservableListBehind()
     {
+        _loadedSubscriptionId = ObservableList.ChangedEvent.Subscribe(EmitChange);
     }
 
     protected ObservableListBehind(ObservableList<T> list)
     {
+        _loadedSubscriptionId = list.ChangedEvent.Subscribe(EmitChange);
         ObservableList = list;
     }
 
     protected ObservableList<T> ObservableList { get; } = [];
+
+    public void Dispose()
+    {
+        ObservableList.ChangedEvent.Unsubscribe(_loadedSubscriptionId);
+        GC.SuppressFinalize(this);
+    }
 }
