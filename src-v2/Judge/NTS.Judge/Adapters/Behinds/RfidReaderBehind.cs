b2e1cacad22439d35c73efc40ab78fe232cdac99
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-10-23 16:01:27 +0300

    clean up code

--- src-v2/Judge/NTS.Judge/Adapters/Behinds/RfidReaderBehind.cs
+++ src-v2/Judge/NTS.Judge/Adapters/Behinds/RfidReaderBehind.cs
@@ -1,14 +1,10 @@
 ﻿using Not.Notifier;
-using Not.Structures;
 using NTS.Domain;
 using NTS.Domain.Core.Configuration;
 using NTS.Domain.Enums;
 using NTS.Domain.Objects;
 using NTS.Judge.Blazor.Ports;
 using NTS.Judge.HardwareControllers;
-using System.Linq;
-using System.Reflection.PortableExecutable;
-using static MudBlazor.CategoryTypes;
 
 namespace NTS.Judge.Adapters.Behinds;
 
@@ -16,8 +12,8 @@ public class RfidReaderBehind : IRfidReaderBehind
 {
     private readonly ISnapshotProcessor _snapshotProcessor;
     private VupVF747pController VF747PController;
-    private Dictionary<int, Timestamp> _deduplication;
-    private Task _readTask;
+    private Dictionary<int, Timestamp> _deduplication = [];
+
     public RfidReaderBehind(ISnapshotProcessor snapshotProcessor)
     {
         VF747PController = new VupVF747pController("192.168.68.128", TimeSpan.FromMilliseconds(10));
@@ -28,17 +24,17 @@ public class RfidReaderBehind : IRfidReaderBehind
     {
         if(!VF747PController.IsConnected || !VF747PController.IsReading)
         {
-            _readTask = Task.Run(VF747PController.StartReading);
-            VF747PController.OnRead += ProcessDetectedTags;
+            Task.Run(VF747PController.StartReading);
+            VF747PController.OnRead += OnRead;
         }
     }
     public void StopReading()
     {
         VF747PController.StopReading();
-        VF747PController.OnRead -= ProcessDetectedTags;
+        VF747PController.OnRead -= OnRead;
     }
 
-    public async void ProcessDetectedTags(object? sender, (DateTime timestamp, string data) e)
+    public async void OnRead(object? sender, (DateTime timestamp, string data) e)
     {
         var number = int.Parse(e.data.Substring(0, 3));
         var timestamp = new Timestamp(e.timestamp);
