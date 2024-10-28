311bafa5fb840e83122e1a98d98c879b9de09ed4
achobanov <alexchobanov@gmail.com>
2024-10-28 19:34:57 +0200

    Remove unnecessary comments

--- src-v2/Not/Not/Filesystem/FilesystemContext.cs
+++ src-v2/Not/Not/Filesystem/FilesystemContext.cs
@@ -4,9 +4,6 @@ public abstract class FilesystemContext
 {
     protected string GetAppDirectory(string subdirectory)
     {
-        //return Path.Combine(
-        //    "D:\\Source\\NTS\\not-timing-system\\src-v2\\Judge\\NTS.Judge.MAUI\\bin\\Release\\net8.0-windows10.0.19041.0\\win10-x64",
-        //    subdirectory);
         var basePath =
 #if DEBUG
             "C:\\tmp\\nts";
