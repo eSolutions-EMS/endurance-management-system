38f4e0c5dfae1bb326cdfa8a2850865e33e4a5f5
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-11-05 17:00:04 +0200

    improve Start.cs ctor logic and clean up code
    https://github.com/Not-Endurance/not-timing-system/pull/280

--- src-v2/NTS.Domain.Core/Objects/Start.cs
+++ src-v2/NTS.Domain.Core/Objects/Start.cs
@@ -15,21 +15,18 @@ public class StartList
             {
                 var phases = participation.Phases;
                 var now = new Timestamp(DateTime.Now);
-                foreach (var phase in phases)
+                foreach (var phase in phases.Where(p=>p.StartTime!=null))
                 {
                     var index = phases.IndexOf(phase);
-                    if (phase.IsComplete())
+                    if (now - phase.StartTime > TimeSpan.FromMinutes(15))
                     {
                         var previousStart = new Start(participation.Combination.Name, participation.Combination.Number, index + 1, phases[index].Length, participation.Phases.Distance, phase.StartTime!);
                         PreviousStarts.Add(previousStart);
                     }
                     else
                     {
-                        if(phase == phases.Current && now - phase.StartTime < TimeSpan.FromMinutes(15))
-                        {
-                            var upcomingStart = new Start(participation.Combination.Name, participation.Combination.Number, index, phases[index].Length, participation.Phases.Distance, phase.StartTime!);
-                            UpcomingStarts.Add(upcomingStart);
-                        }
+                        var upcomingStart = new Start(participation.Combination.Name, participation.Combination.Number, index + 1, phases[index].Length, participation.Phases.Distance, phase.StartTime!);
+                        UpcomingStarts.Add(upcomingStart);
                     }
                 }
             }
@@ -37,43 +34,16 @@ public class StartList
     }
     public List<Start> UpcomingStarts { get; set; } = new List<Start>();
     public List<Start> PreviousStarts { get; set; } = new List<Start>();
-
-    public IEnumerable<Start> GroupByDistanceAndAthlete()
-    {
-        var group1 = PreviousStarts
-            .GroupBy(sh => Tuple.Create(sh.TotalDistance, sh.Athlete));
-        var sort1 = group1
-            .OrderBy(g => g.Key.Item1);
-        var sort2 = sort1
-            .ThenBy(g => g.Key.Item2); //returns IOrderedEnumerable<IGrouping<Tuple<double, Person>, Start>>
-        var group = sort2
-            .FlattenGroupedItems();
-        return group;
-    }
 }
-public class Start : IComparable<Start>
+public class Start
 {
-    public Start(Participation participation)
-    {
-        var phases = participation.Phases;
-        Athlete = participation.Combination.Name;
-        Number = participation.Combination.Number;
-        PhaseNumber = phases.IndexOf(phases.Current)+1;
-        Distance = participation.Phases.Current.Length;
-        StartAt = participation.Phases.Current.StartTime;
-        GuardHelper.ThrowIfDefault(participation?.Phases?.Current);
-        //CurrentPhase = participation?.Phases?.Current;
-        TotalDistance = participation!.Phases!.Distance;
-    }
-
     public Start(Person athlete, int number, int loopNumber, double distance, double totalDistance, Timestamp startAt)
     {
         Athlete = athlete;
         Number = number;
         PhaseNumber = loopNumber;
         Distance = distance;
-        TotalDistance = totalDistance;
-        //CurrentPhase = phase;
+        TotalDistance = totalDistance.FloorWholeNumberToTens();
         StartAt = startAt;
     }
 
@@ -82,7 +52,6 @@ public class Start : IComparable<Start>
     public int PhaseNumber { get; private set; }
     public double Distance { get; private set; }
     public double TotalDistance { get; private set; }
-    //public Phase CurrentPhase { get; private set; }
     public Timestamp? StartAt { get; private set; }
 
     public string StartIn()
@@ -96,27 +65,11 @@ public class Start : IComparable<Start>
         {
             return "-" + (StartAt - Timestamp.Now()).ToString();
         }
-        //test cases -remove before merge
-        //return "00:05:02";
-        //return "00:00:03";
-        //return "-00:14:57";
     }
 
     public override string ToString()
     {
         var result = $"{Number}, {Athlete}, {"#".Localize()}{PhaseNumber}: {Distance}km, {StartAt}";
         return result;
-    }
-
-    public int CompareTo(Start other)
-    {
-        if (other == null) return 1;
-
-        // First compare Athlete
-        int athleteComparison = string.Compare(Athlete, other.Athlete, StringComparison.Ordinal);
-        if (athleteComparison != 0) return athleteComparison;
-
-        // Then compare Competition Distance if Athlete is the same
-        return string.Compare(TotalDistance.ToString(), other.TotalDistance.ToString(), StringComparison.Ordinal);
     }
 }
