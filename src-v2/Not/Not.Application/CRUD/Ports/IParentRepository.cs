2ddb325201f093d7d00be6fe651fce09ae863496
achobanov <alexchobanov@gmail.com>
2024-11-11 16:56:01 +0200

    Removed obsolete Child/Parent CRUD ports
    https://github.com/Not-Endurance/not-timing-system/issues/321

--- src-v2/Not/Not.Application/CRUD/Ports/IParentRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿using Not.Domain;
-
-namespace Not.Application.CRUD.Ports;
-
-public interface IParentRepository<T> : ICreateChild<T>, IUpdateChild<T>, IDeleteChild<T>
-    where T : DomainEntity
-{ }
