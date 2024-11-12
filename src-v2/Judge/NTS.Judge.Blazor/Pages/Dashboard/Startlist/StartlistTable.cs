02346e2b6ddb6e3d65ea24477434244c4825ddae
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-11-12 22:00:35 +0200

    extract common functionality of Upcoming and History in StartlistTable

---
+++ src-v2/Judge/NTS.Judge.Blazor/Pages/Dashboard/Startlist/StartlistTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MudBlazor;
+using Not.Blazor.Components;
+using Not.Blazor.TM.Models;
+using Not.Services;
+using NTS.Domain.Core.Objects;
+
+namespace NTS.Judge.Blazor.Pages.Dashboard.Startlist;
+public abstract class StartlistTable : NotComponent
+{ 
+    protected MudTabs _tabs = default!;
+    protected List<TabModel> _userTabs = [];
+    [Parameter]
+    public IEnumerable<Start> Starts { get; set; } = [];
+
+    protected override void OnParametersSet()
+    {
+        foreach (var start in Starts)
+        {
+            var tabId = Guid.NewGuid();
+            var tabHeader = $"{@Localizer.Get("Gate")} {start.PhaseNumber}";
+            var tab = new TabModel(tabId, tabHeader);
+            if (!_userTabs.Any(t => t.Header == tab.Header))
+            {
+                _userTabs.Add(tab);
+            }
+        }
+    }
+}
