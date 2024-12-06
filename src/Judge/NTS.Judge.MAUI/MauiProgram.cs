609eb19dbda56c134396dbfe058487e7c7b9de88
achobanov <alexchobanov@gmail.com>
2024-12-06 22:16:59 +0200

    Start Judge.MAUI.Server using Process.Start
    https://github.com/Not-Endurance/not-timing-system/issues/322

--- src/Judge/NTS.Judge.MAUI/MauiProgram.cs
+++ src/Judge/NTS.Judge.MAUI/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
 using Not.Filesystem;
 using Not.Injection;
 using NTS.Judge.MAUI.Injection;
@@ -17,7 +18,34 @@ public static class MauiProgram
 
         var app = builder.Build();
 
+        StartHub();
 
         return app;
+    }
+
+    static void StartHub()
+    {
+        try
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var info = new ProcessStartInfo
+            {
+                FileName = Path.Combine(currentDirectory, "NTS.Judge.MAUI.Server.exe"),
+            };
+
+            var hubProcess = Process.Start(info);
+            AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+            {
+                if (hubProcess != null && !hubProcess.HasExited)
+                {
+                    hubProcess.CloseMainWindow();
+                }
+            };
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        
     }
 }
