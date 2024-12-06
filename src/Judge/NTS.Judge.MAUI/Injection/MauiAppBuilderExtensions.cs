fdea0fcb0f6321edcc75cb3a310cbad4fa17038e
achobanov <alexchobanov@gmail.com>
2024-12-06 21:41:20 +0200

    Extract MauiAppBuilderExtensions
    https://github.com/Not-Endurance/not-timing-system/issues/322

---
+++ src/Judge/NTS.Judge.MAUI/Injection/MauiAppBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using Not.MAUI.Logging;
+using NTS.Judge.Blazor;
+using NTS.Judge.Shared;
+using NTS.Storage.Injection;
+using Not.Filesystem;
+using Not.Logging.Builder;
+using Not.Injection;
+
+namespace NTS.Judge.MAUI.Injection;
+
+public static class MauiAppBuilderExtensions
+{
+    public static MauiAppBuilder ConfigureBlazor(this MauiAppBuilder builder)
+    {
+        builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+        builder.Logging.AddDebug();
+        builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+        FileContextHelper.SetDebugRootDirectory("nts");
+        builder.ConfigureLogging().AddFilesystemLogger();
+
+        builder.Services
+            .AddJudgeBlazor(builder.Configuration)
+            .AddInversedDependencies();
+
+        return builder;
+    }
+
+    public static IServiceCollection AddInversedDependencies(this IServiceCollection services)
+    {
+        services.AddStorage().AddJudge().GetConventionalAssemblies().RegisterConventionalServices();
+        return services;
+    }
+}
--- src/Judge/NTS.Judge.MAUI/MauiProgram.cs
+++ src/Judge/NTS.Judge.MAUI/MauiProgram.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using Not.Filesystem;
 using Not.Injection;
-using Not.Logging.Builder;
-using Not.MAUI.Logging;
-using NTS.Judge.Blazor;
-using NTS.Judge.Shared;
-using NTS.Storage.Injection;
+using NTS.Judge.MAUI.Injection;
 
 namespace NTS.Judge.MAUI;
 
@@ -19,32 +15,9 @@ public static class MauiProgram
             .ConfigureFonts(fonts => fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"))
             .ConfigureBlazor();
 
-        return builder.Build();
-    }
-}
+        var app = builder.Build();
 
-public static class ServiceCollectionExtensions
-{
-    public static MauiAppBuilder ConfigureBlazor(this MauiAppBuilder builder)
-    {
-        builder.Services.AddMauiBlazorWebView();
-#if DEBUG
-        builder.Logging.AddDebug();
-        builder.Services.AddBlazorWebViewDeveloperTools();
-#endif
-        FileContextHelper.SetDebugRootDirectory("nts");
-        builder.ConfigureLogging().AddFilesystemLogger();
 
-        builder.Services
-            .AddJudgeBlazor(builder.Configuration)
-            .AddInversedDependencies();
-
-        return builder;
-    }
-
-    public static IServiceCollection AddInversedDependencies(this IServiceCollection services)
-    {
-        services.AddStorage().AddJudge().GetConventionalAssemblies().RegisterConventionalServices();
-        return services;
+        return app;
     }
 }
