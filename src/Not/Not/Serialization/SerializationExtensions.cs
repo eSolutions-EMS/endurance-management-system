3d3cca626cb68bcd46007e33be35ab21ddf7458c
achobanov <alexchobanov@gmail.com>
2025-01-04 12:39:34 +0200

    Ensure serialization/deserialization and ResetConveerters is paired
    Without locks a converter's Reset method can be exectued from a nother
    call while it'sserializaing/deserializing which obvious fails
    
    https://github.com/Not-Endurance/not-timing-system/issues/322

--- src/Not/Not/Serialization/SerializationExtensions.cs
+++ src/Not/Not/Serialization/SerializationExtensions.cs
@@ -23,23 +23,30 @@ public static class SerializationExtensions
 
     public static string ToJson(this object obj)
     {
-        var result = JsonConvert.SerializeObject(obj, SETTINGS);
-        ResetConverters();
-        return result;
+        lock (_lock)
+        {
+            var result = JsonConvert.SerializeObject(obj, SETTINGS);
+            ResetConverters();
+            return result;
+        }
     }
 
     public static T FromJson<T>(this string json)
         where T : class
     {
-        var result = JsonConvert.DeserializeObject<T>(json, SETTINGS);
-        if (result == default)
+        lock (_lock) // TODO: investigate serialization locking as a whole (reference serializer locks as well)
         {
-            throw new Exception($"Cannot serialize '{json}' to type of '{typeof(T)}'");
+            var result = JsonConvert.DeserializeObject<T>(json, SETTINGS);
+            if (result == default)
+            {
+                throw new Exception($"Cannot serialize '{json}' to type of '{typeof(T)}'");
+            }
+            ResetConverters();
+            return result;
         }
-        ResetConverters();
-        return result;
     }
 
+    static object _lock = new();
     static List<JsonConverterBase> _converters = [];
 
     static void ResetConverters()
